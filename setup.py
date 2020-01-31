70f83fe85421d6c6d293e2358b83dee3ec11cc38
Jeremy McGibbon <jeremym@vulcan.com>
2020-01-31 10:52:06 -0800

    set fv3util version to match python wrapper

--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ setup(
     packages=['fv3util'],
     include_package_data=True,
     url='https://github.com/VulcanClimateModeling/fv3gfs-python',
-    version='0.2.1',
+    version='0.3.0',
     zip_safe=False,
 )
