ea5b27e14369fdbeb4a3d6916a7202d2655ebd52
Jeremy McGibbon <jeremym@vulcan.com>
2020-02-17 14:59:40 -0800

    updating docs

--- fv3util/mpi.py
+++
@@ -1,9 +0,0 @@
-
-
-def get_tile_number(tile_rank, total_ranks):
-    """Returns the tile number for a given rank and total number of ranks.
-    """
-    if total_ranks % 6 != 0:
-        raise ValueError(f'total_ranks {total_ranks} is not evenly divisible by 6')
-    ranks_per_tile = total_ranks // 6
-    return tile_rank // ranks_per_tile + 1
