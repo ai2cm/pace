d62494c591d85e2ce60b6435327a00d566192ca3
Rhea George <RheaG@Rheas-MacBook-Pro.local>
2020-03-01 19:43:04 -0800

    missing part of last review, uncommitted

--- make_rundir.py
+++ make_rundir.py
@@ -2,5 +2,5 @@ import fv3config
 import yaml
 import os
 fv3config.ensure_data_is_downloaded()
-config = yaml.safe_load(open('serialize.yml', 'r'))
+config = yaml.safe_load(open('fv3config.yml', 'r'))
 fv3config.write_run_directory(config, '/rundir')
