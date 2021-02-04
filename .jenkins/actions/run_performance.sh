709cccd3b3b6b55c46f4ed5a3bc434d8650370fa
Tobias Wicky <wicky.tob@gmail.com>
2021-02-04 14:22:27 +0100

    add 128 data path and run for fewer timesteps (#215)
    Purpose
    
    This PR adds the working c128 dataset to the jenkins-script for performance checks.
    Code changes:
    
        The .jenkins files now also point to c128 data if this is the experiment chosen
    
    Infrastructure changes:
    
        The only change is we're enabling c128 tests in <jenkins>job/fv3core-performance-test/

--- .jenkins/actions/run_performance.sh
+++ .jenkins/actions/run_performance.sh
@@ -3,14 +3,19 @@ set -e -x
 SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 ROOT_DIR="$(dirname "$(dirname "$SCRIPTPATH")")"
+TESTDATA_PATH="/project/s1053/fv3core_serialized_test_data/"
 test -n "${experiment}" || exitError 1001 ${LINENO} "experiment is not defined"
 test -n "${backend}" || exitError 1002 ${LINENO} "backend is not defined"
 
-if [ "$experiment" = "c96_6ranks_baroclinic" ]; then
-    data_path="/project/s1053/fv3core_serialized_test_data/7.2.3/c96_6ranks_baroclinic"
-fi
+
 if [ "$experiment" = "c12_6ranks_standard" ]; then
-    data_path="/project/s1053/fv3core_serialized_test_data/7.0.0/c12_6ranks_standard"
+    data_path="${TESTDATA_PATH}7.0.0/c12_6ranks_standard"
+fi
+if [ "$experiment" = "c96_6ranks_baroclinic" ]; then
+    data_path="${TESTDATA_PATH}7.2.3/c96_6ranks_baroclinic"
+fi
+if [ "$experiment" = "c128_6ranks_baroclinic" ]; then
+    data_path="${TESTDATA_PATH}7.2.3/c128_6ranks_baroclinic"
 fi
 
-$ROOT_DIR/examples/standalone/benchmarks/run_on_daint.sh 60 6 $backend /project/s1053/performance/fv3core_monitor/$backend/ $data_path
+$ROOT_DIR/examples/standalone/benchmarks/run_on_daint.sh 2 6 $backend /project/s1053/performance/fv3core_monitor/$backend/ $data_path
