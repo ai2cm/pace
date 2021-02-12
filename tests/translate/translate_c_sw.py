1434dfd237768691deca981103367b377c9a923c
eddie-c-davis <22508135+eddie-c-davis@users.noreply.github.com>
2021-02-12 11:02:34 -0800

    Move 'dt2' to end of 'update_vorticity_and_kinetic_energy_stencil' (#239)

--- tests/translate/translate_c_sw.py
+++ tests/translate/translate_c_sw.py
@@ -244,9 +244,9 @@ def update_vorticity_and_kinetic_energy_stencil(
     cos_sg3: FloatField,
     sin_sg4: FloatField,
     cos_sg4: FloatField,
-    dt2: float,
     ke_c: FloatField,
     vort_c: FloatField,
+    dt2: float,
 ):
     with computation(PARALLEL), interval(...):
         ke_c, vort_c = update_vorticity_and_kinetic_energy(
