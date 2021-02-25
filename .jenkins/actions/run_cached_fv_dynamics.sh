5cdc8b782fba53caf670e4b13be75207d4fb7aa6
Tobias Wicky <wicky.tob@gmail.com>
2021-02-25 20:09:30 +0100

    move to the new generated caches (#267)
    Co-authored-by: tobias <twicki12@gmail.com>

--- .jenkins/actions/run_cached_fv_dynamics.sh
+++ .jenkins/actions/run_cached_fv_dynamics.sh
@@ -9,6 +9,6 @@ make get_test_data
 
 if [ ! -d $(pwd)/.gt_cache_000000 ]; then
     cp -r /scratch/snx3000/olifu/jenkins/scratch/store_gt_caches/$EXPNAME/$BACKEND/.gt_cache_0000* .
-    find . -name m_\*.py -exec sed -i "s|\/scratch\/snx3000\/olifu\/jenkins_submit\/workspace\/zz_fv3core_cacheSetup\/backend\/$BACKEND\/experiment\/$EXPNAME\/slave\/daint_submit|$(pwd)|g" {} +
+    find . -name m_\*.py -exec sed -i "s|\/scratch\/snx3000\/olifu\/jenkins_submit\/workspace\/fv3core-cache-setup\/backend\/$BACKEND\/experiment\/$EXPNAME\/slave\/daint_submit|$(pwd)|g" {} +
 fi
 make tests_venv_mpi
