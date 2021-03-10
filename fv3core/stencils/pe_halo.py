d77c3b71605468eaaafa52da891cca189358ea30
Christopher Kung <68066130+gmao-ckung@users.noreply.github.com>
2021-03-10 10:41:26 -0500

    Combine pe_halo.py stencils by using horizontal regions (#287)
    * Applied horizontal regions to pe_halo.py to reduce number of stencil calls from 4 to 1.
    
    * Small tweak to regions bounds in 'i'
    
    * Used single index for size 1 length slices in horizontal regions
    
    * Replaced "global" horizontal region variables  in pe_halo with "local" horizontal region variables to properly address compute bounds for multiple ranks

--- fv3core/stencils/pe_halo.py
+++ fv3core/stencils/pe_halo.py
@@ -1,27 +1,39 @@
-from gt4py.gtscript import FORWARD, computation, interval
+from gt4py.gtscript import FORWARD, computation, horizontal, interval, region
 
 import fv3core._config as spec
-import fv3core.utils.gt4py_utils as utils
 from fv3core.decorators import gtstencil
-
-
-sd = utils.sd
+from fv3core.utils.typing import FloatField
 
 
 @gtstencil()
-def edge_pe(pe: sd, delp: sd, ptop: float):
+def edge_pe(pe: FloatField, delp: FloatField, ptop: float):
+    from __externals__ import local_ie, local_is, local_je, local_js
+
     with computation(FORWARD):
         with interval(0, 1):
-            pe[0, 0, 0] = ptop
+            with horizontal(
+                region[local_is - 1, local_js : local_je + 1],
+                region[local_ie + 1, local_js : local_je + 1],
+                region[local_is - 1 : local_ie + 2, local_js - 1],
+                region[local_is - 1 : local_ie + 2, local_je + 1],
+            ):
+                pe[0, 0, 0] = ptop
         with interval(1, None):
-            pe[0, 0, 0] = pe[0, 0, -1] + delp[0, 0, -1]
+            with horizontal(
+                region[local_is - 1, local_js : local_je + 1],
+                region[local_ie + 1, local_js : local_je + 1],
+                region[local_is - 1 : local_ie + 2, local_js - 1],
+                region[local_is - 1 : local_ie + 2, local_je + 1],
+            ):
+                pe[0, 0, 0] = pe[0, 0, -1] + delp[0, 0, -1]
 
 
 def compute(pe, delp, ptop):
     grid = spec.grid
-    edge_domain_x = (1, grid.njc, grid.npz + 1)
-    edge_pe(pe, delp, ptop, origin=(grid.is_ - 1, grid.js, 0), domain=edge_domain_x)
-    edge_pe(pe, delp, ptop, origin=(grid.ie + 1, grid.js, 0), domain=edge_domain_x)
-    edge_domain_y = (grid.nic + 2, 1, grid.npz + 1)
-    edge_pe(pe, delp, ptop, origin=(grid.is_ - 1, grid.js - 1, 0), domain=edge_domain_y)
-    edge_pe(pe, delp, ptop, origin=(grid.is_ - 1, grid.je + 1, 0), domain=edge_domain_y)
+    edge_pe(
+        pe,
+        delp,
+        ptop,
+        origin=grid.full_origin(),
+        domain=grid.domain_shape_full(add=(0, 0, 1)),
+    )
