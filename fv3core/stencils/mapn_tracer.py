554ac2b4bc8109ffab2745ed60ac3a741b99b3b6
oelbert <oliver.elbert36@gmail.com>
2021-03-30 12:14:51 -0400

    Seting tracer remapping parameters in a stencil
    Improves performance by setting the initial values of the interpolation parameters inside a stencil rather than in Python code.
    
    - added a set_components stencil to mapn_tracer that sets the initial values of the 4 interpolation fields, rather than doing this in Python.

--- fv3core/stencils/mapn_tracer.py
+++ fv3core/stencils/mapn_tracer.py
@@ -1,11 +1,29 @@
 from typing import Dict, Optional
 
+from gt4py.gtscript import PARALLEL, computation, interval
+
 import fv3core._config as spec
 import fv3core.stencils.fillz as fillz
 import fv3core.stencils.map_single as map_single
 import fv3core.stencils.remap_profile as remap_profile
 import fv3core.utils.gt4py_utils as utils
+from fv3core.decorators import gtstencil
 from fv3core.utils.typing import FloatField
+
+
+@gtstencil
+def set_components(
+    tracer: FloatField,
+    a4_1: FloatField,
+    a4_2: FloatField,
+    a4_3: FloatField,
+    a4_4: FloatField,
+):
+    with computation(PARALLEL), interval(...):
+        a4_1 = tracer
+        a4_2 = 0.0
+        a4_3 = 0.0
+        a4_4 = 0.0
 
 
 def compute(
@@ -21,6 +39,12 @@ def compute(
     j_2d: Optional[int] = None,
     version: str = "stencil",
 ):
+    domain_compute = (
+        spec.grid.ie - spec.grid.is_ + 1,
+        spec.grid.je - spec.grid.js + 1,
+        spec.grid.npz + 1,
+    )
+
     qs = utils.make_storage_from_shape(pe1.shape, origin=(0, 0, 0))
     (
         dp1,
@@ -37,10 +61,16 @@ def compute(
     # transliterated fortran 3d or 2d validate, not bit-for bit
     tracer_list = [tracers[q] for q in utils.tracer_variables[0:nq]]
     for tracer in tracer_list:
-        q4_1[:] = tracer[:]
-        q4_2[:] = 0.0
-        q4_3[:] = 0.0
-        q4_4[:] = 0.0
+        set_components(
+            tracer,
+            q4_1,
+            q4_2,
+            q4_3,
+            q4_4,
+            origin=(spec.grid.is_, spec.grid.js, 0),
+            domain=domain_compute,
+        )
+
         q4_1, q4_2, q4_3, q4_4 = remap_profile.compute(
             qs,
             q4_1,
