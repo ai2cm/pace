d93aa01490c6bdfdc51c2ad9100212453ee3751a
rheacangeo <55423144+rheacangeo@users.noreply.github.com>
2021-03-31 13:56:12 -0700

    adding set and get validate_args to the fv3core/__init__ (#354)

--- fv3core/__init__.py
+++ fv3core/__init__.py
@@ -2,4 +2,11 @@
 from .decorators import disable_stencil_report, enable_stencil_report
 from .stencils.fv_dynamics import DynamicalCore, fv_dynamics
 from .stencils.fv_subgridz import compute as fv_subgridz
-from .utils.global_config import get_backend, get_rebuild, set_backend, set_rebuild
+from .utils.global_config import (
+    get_backend,
+    get_rebuild,
+    get_validate_args,
+    set_backend,
+    set_rebuild,
+    set_validate_args,
+)
