58439d432a0a45536aff44941af0f0a36e0acd5e
rheacangeo <55423144+rheacangeo@users.noreply.github.com>
2021-05-03 11:43:27 -0700

    hotfix to ensure the standalone still runs with the inputs needed for neg_adj3 as an object (#447)
    hotfix to ensure the standalone still runs with the inputs needed for neg_adj3 as an object

--- examples/standalone/runfile/dynamics.py
+++ examples/standalone/runfile/dynamics.py
@@ -171,8 +171,8 @@ if __name__ == "__main__":
             state["atmosphere_hybrid_a_coordinate"],
             state["atmosphere_hybrid_b_coordinate"],
             state["surface_geopotential"],
-            state["specific_humidity"],
-            state["graupel_mixing_ratio"],
+            state["specific_humidity"].data,
+            state["graupel_mixing_ratio"].data,
         )
 
         # warm-up timestep.
