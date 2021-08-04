bf0edf880a3cd7e942b125030ba01ce608a472a2
Chris Kung <christopher.w.kung@nasa.gov>
2021-08-04 18:05:19 -0400

    Got values from get_phi_fv3 from the integration to verify with the standalone

--- fv3gfs/physics/stencils/get_phi_fv3.py
+++ fv3gfs/physics/stencils/get_phi_fv3.py
@@ -41,22 +41,22 @@ def get_phi_fv3_stencil(
         with interval(0, 1):
             phii = phii[0, 0, 1] + del_gz[0, 0, 1]
 
-
 def get_phi_fv3(
     gt0: FIELD_FLT, gq0: FIELD_FLT, del_gz: FIELD_FLT, phii: FIELD_FLT, phil: FIELD_FLT
 ):
-    with computation(BACKWARD), interval(-1, None):
-        phii = 0.0
-
-    with computation(PARALLEL), interval(0, -1):
+    with computation(PARALLEL), interval(0,-1):
         del_gz = (
-            del_gz[0, 0, 0] * gt0[0, 0, 0] * (1.0 + con_fvirt * max(0.0, gq0[0, 0, 0]))
-        )
+                del_gz[0, 0, 0]
+                * gt0[0, 0, 0]
+                * (1.0 + con_fvirt * max(0.0, gq0[0, 0, 0]))
+            )
 
-    # [TODO] For c12 baroclinic mp, phii passes without any changes.
-    # But why isn't this passing?
-    # with computation(BACKWARD), interval(0, -1):
-    #     phii = phii[0, 0, 1] + del_gz[0, 0, 1]
-
-    with computation(PARALLEL), interval(0, -1):
-        phil = 0.5 * (phii[0, 0, 0] + phii[0, 0, 1])
+    with computation(BACKWARD):
+        with interval(-1, None):
+            phii = 0.0
+        with interval(-2,-1):
+            phil = 0.5 * (phii[0, 0, 1] + phii[0, 0, 1] + del_gz[0, 0, 0])
+            phii = phii[0, 0, 1] + del_gz[0, 0, 0]
+        with interval(0, -2):            
+            phil = 0.5 * (phii[0, 0, 1] + phii[0, 0, 1] + del_gz[0, 0, 0])
+            phii = phii[0, 0, 1] + del_gz[0, 0, 0]
