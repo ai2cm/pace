5ca6cfb2a06736083b576fa19c917a79d7a5cdea
Elynn Wu <elynn510@gmail.com>
2021-09-08 14:20:42 -0700

    PhiFV3 savepoint test added

--- tests/savepoint/translate/__init__.py
+++ tests/savepoint/translate/__init__.py
@@ -1,2 +1,3 @@
 # flake8: noqa: F401
 from .translate_prsfv3 import TranslatePrsFV3
+from .translate_phifv3 import TranslatePhiFV3
--- tests/savepoint/translate/translate_phifv3.py
+++ tests/savepoint/translate/translate_phifv3.py
@@ -1,34 +1,38 @@
-from fv3gfs.physics.stencils.get_prs_fv3 import get_prs_fv3
+from fv3gfs.physics.stencils.get_phi_fv3 import get_phi_fv3
 from fv3gfs.physics.testing import TranslatePhysicsFortranData2Py
 import fv3core._config as spec
 from fv3core.decorators import FrozenStencil
 
 
-class TranslatePrsFV3(TranslatePhysicsFortranData2Py):
+class TranslatePhiFV3(TranslatePhysicsFortranData2Py):
     def __init__(self, grid):
         super().__init__(grid)
 
         self.in_vars["data_vars"] = {
-            "phii": {"serialname": "prs_phii"},
-            "prsi": {"serialname": "prs_prsi"},
-            "tgrs": {"serialname": "prs_tgrs"},
-            "qgrs": {"serialname": "prs_qgrs"},
-            "del_": {"serialname": "prs_del", "kend": grid.npz - 1},
-            "del_gz": {"serialname": "prs_del_gz", "out_roll_zero": True},
+            "gt0": {"serialname": "phi_gt0"},
+            "gq0": {"serialname": "phi_gq0"},
+            "del_gz": {
+                "serialname": "phi_del_gz",
+                "in_roll_zero": True,
+                "out_roll_zero": True,
+            },
+            "phii": {"serialname": "phi_phii"},
+            "phil": {"serialname": "phi_phil", "kend": grid.npz - 1},
         }
         self.out_vars = {
-            "del_": self.in_vars["data_vars"]["del_"],
             "del_gz": self.in_vars["data_vars"]["del_gz"],
+            "phii": self.in_vars["data_vars"]["phii"],
+            "phil": self.in_vars["data_vars"]["phil"],
         }
         self.compute_func = FrozenStencil(
-            get_prs_fv3,
+            get_phi_fv3,
             origin=self.grid.grid_indexing.origin_full(),
             domain=self.grid.grid_indexing.domain_full(add=(0, 0, 1)),
         )
 
     def compute(self, inputs):
         self.make_storage_data_input_vars(inputs)
-        inputs["qgrs"] = inputs["qgrs"]["qvapor"]
+        inputs["gq0"] = inputs["gq0"]["qvapor"]
         self.compute_func(**inputs)
         return self.slice_output(inputs)
 
