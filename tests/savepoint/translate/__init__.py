d08c1788a7c5ab07a8b666432eadc5c88ab3742e
Elynn Wu <elynn510@gmail.com>
2021-09-16 16:31:02 -0700

    PhysicsDriver sp test passing with loose threshold

--- tests/savepoint/translate/__init__.py
+++ tests/savepoint/translate/__init__.py
@@ -3,3 +3,4 @@ from .translate_prsfv3 import TranslatePrsFV3
 from .translate_phifv3 import TranslatePhiFV3
 from .translate_atmos_phy_statein import TranslateAtmosPhysDriverStatein
 from .translate_fillgfs import TranslateFillGFS
+from .translate_gfs_physics_driver import TranslateGFSPhysicsDriver
