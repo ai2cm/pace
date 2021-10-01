48c2d386b223cc7554982290c598274b9f81325c
Elynn Wu <elynn510@gmail.com>
2021-10-01 16:52:05 -0700

    fv_update_phys passing with relaxed threshold

--- tests/savepoint/translate/__init__.py
+++ tests/savepoint/translate/__init__.py
@@ -10,4 +10,4 @@ from .translate_update_tracers_phys import TranslatePhysUpdateTracers
 from .translate_update_pressure_sfc_winds_phys import (
     TranslatePhysUpdatePressureSurfaceWinds,
 )
-
+from .translate_fv_update_phys import TranslateFVUpdatePhys
