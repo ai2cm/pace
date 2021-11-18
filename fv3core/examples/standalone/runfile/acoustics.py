881dac6eea8e8e2e963bd6153233db2e447372bc
Rhea George <rheag@allenai.org>
2021-11-18 13:30:38 -0800

    keeping the acoustics read grid method, which could come in handy for launching simulations that are not baroclinic

--- fv3core/examples/standalone/runfile/acoustics.py
+++ fv3core/examples/standalone/runfile/acoustics.py
@@ -38,6 +38,19 @@ def initialize_serializer(data_directory: str, rank: int = 0) -> serialbox.Seria
         "Generator_rank" + str(rank),
     )
 
+# Only used if reading the grid from savepoints on disk
+# Default is to generate abaroclinic initialization
+def read_grid(serializer: serialbox.Serializer, rank: int = 0) -> Grid:
+    """Uses the serializer to generate a Grid object from serialized data"""
+    grid_savepoint = serializer.get_savepoint("Grid-Info")[0]
+    grid_data = {}
+    grid_fields = serializer.fields_at_savepoint(grid_savepoint)
+    for field in grid_fields:
+        grid_data[field] = serializer.read(field, grid_savepoint)
+        if len(grid_data[field].flatten()) == 1:
+            grid_data[field] = grid_data[field][0]
+    return fv3core.testing.TranslateGrid(grid_data, rank).python_grid()
+
 
 def initialize_fv3core(backend: str, disable_halo_exchange: bool) -> None:
     """
