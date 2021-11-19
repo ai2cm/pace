aea26ad7cbd28b6b5750792d6fe40731b5c1f8f5
Elynn Wu <elynn510@gmail.com>
2021-11-19 11:35:06 -0800

    fix jenkins

--- .jenkins/actions/run_physics_regression_tests.sh
+++ .jenkins/actions/run_physics_regression_tests.sh
@@ -8,8 +8,8 @@ export TEST_ARGS="-v -s -rsx --backend=${BACKEND} "
 
 if [ ${python_env} == "virtualenv" ]; then
     export TEST_ARGS="${TEST_ARGS} --junitxml=${jenkins_dir}/${XML_REPORT}"
-    CONTAINER_CMD="srun" make tests physics_savepoint_tests
+    CONTAINER_CMD="srun" make physics_savepoint_tests
 else
     export TEST_ARGS="${TEST_ARGS} --junitxml=/${XML_REPORT}"
-    make tests physics_savepoint_tests
+    make physics_savepoint_tests
 fi
