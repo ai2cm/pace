a50c4cd0518718aeba9850f625248cc9a7c85808
Jeremy McGibbon <mcgibbon@uw.edu>
2021-11-19 11:47:40 -0800

    remove some references to GT4PY_VERSION.txt

--- .jenkins/install_virtualenv.sh
+++ .jenkins/install_virtualenv.sh
@@ -11,14 +11,15 @@ exitError()
 
 # check a virtualenv path has been provided
 test -n "$1" || exitError 1001 ${virtualenv_path} "must pass an argument"
-wheel_dir=/project/s1053/install/wheeldir
-wheel_command="--find-links=$wheel_dir"
-make update_submodules_venv
-virtualenv_path=$1
 pace_dir=`dirname $0`/../
 echo "pace path is ${pace_dir}"
+wheel_dir=/project/s1053/install/wheeldir
+wheel_command="--find-links=$wheel_dir"
+virtualenv_path=$1
+git submodule update --init ${pace_dir}/external/daint_venv
+git submodule update --init ${pace_dir}/external/gt4py
 if [ -z "${GT4PY_VERSION}" ]; then
-    export GT4PY_VERSION=`cat ${pace_dir}/GT4PY_VERSION.txt`
+    export GT4PY_VERSION=git submodule status ${pace_dir}/external/gt4py | awk '{print $1;}'
 fi
 (cd ${pace_dir}/external/daint_venv && ./install.sh ${virtualenv_path})
 source ${virtualenv_path}/bin/activate
@@ -26,4 +27,5 @@ python3 -m pip install ${pace_dir}/fv3gfs-util/
 python3 -m pip install $wheel_command -c ${pace_dir}/constraints.txt -r fv3core/requirements/requirements_daint.txt
 python3 -m pip install ${PACE_INSTALL_FLAGS} ${pace_dir}/fv3core/
 python3 -m pip install ${PACE_INSTALL_FLAGS} ${pace_dir}/fv3gfs-physics/
+python3 -m pip install ${PACE_INSTALL_FLAGS} ${pace_dir}/external/gt4py/
 deactivate
