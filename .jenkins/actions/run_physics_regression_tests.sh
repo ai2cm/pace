9949cf4afb9549db55628453f6300d6671279372
Elynn Wu <elynn510@gmail.com>
2021-11-19 09:39:56 -0800

    fixed jenkins get data

--- .jenkins/actions/run_physics_regression_tests.sh
+++ .jenkins/actions/run_physics_regression_tests.sh
@@ -5,8 +5,6 @@ EXPNAME=$2
 XML_REPORT="sequential_test_results.xml"
 export TEST_ARGS="-v -s -rsx --backend=${BACKEND} "
 
-# sync the test data
-make get_test_data
 
 if [ ${python_env} == "virtualenv" ]; then
     export TEST_ARGS="${TEST_ARGS} --junitxml=${jenkins_dir}/${XML_REPORT}"
