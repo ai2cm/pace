38dc68227bc0f01ce87e0181d44ee8dbcceff47f
Elynn Wu <elynn510@gmail.com>
2021-11-18 17:32:35 -0800

    install pre-commit

--- .jenkins/actions/lint.sh
+++ .jenkins/actions/lint.sh
@@ -3,7 +3,7 @@ set -e -x
 env_name=venv-${BUILD_NUMBER:-0}
 python3 -m venv ${env_name}
 . ${env_name}/bin/activate
-pip install -r fv3core/requirements/requirements_lint.txt -c constraints.txt
+pip install pre-commit
 pre-commit run --all-files
 deactivate
 echo $(date) > aggregate
