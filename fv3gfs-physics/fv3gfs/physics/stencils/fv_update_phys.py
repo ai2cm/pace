8b0ed9601c1b8dd831197b5326fd51aef1175405
Elynn Wu <elynn510@gmail.com>
2021-11-22 13:21:26 -0800

    lint

--- fv3gfs-physics/fv3gfs/physics/stencils/fv_update_phys.py
+++ fv3gfs-physics/fv3gfs/physics/stencils/fv_update_phys.py
@@ -10,7 +10,6 @@ from gt4py.gtscript import (
     log,
     region,
 )
-from pace.stencils.c2l_ord import CubedToLatLon
 
 import fv3core.utils.global_config as global_config
 import fv3core.utils.gt4py_utils as utils
@@ -23,6 +22,7 @@ from fv3gfs.physics.global_constants import *
 # TODO: we don't want to import from fv3core
 from fv3gfs.physics.stencils.update_dwind_phys import AGrid2DGridPhysics
 from fv3gfs.util.quantity import Quantity
+from pace.stencils.c2l_ord import CubedToLatLon
 
 
 # TODO: This is the same as moist_cv.py in fv3core, should move to integration dir
