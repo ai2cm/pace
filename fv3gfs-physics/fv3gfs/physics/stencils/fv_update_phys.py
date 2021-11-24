2088c518015cbbdf9457a35ccf8dad8edde4ef90
Florian Deconinck <deconinck.florian@gmail.com>
2021-11-24 22:09:12 +0100

    Cleaning up dead code + unused stencils

--- fv3gfs-physics/fv3gfs/physics/stencils/fv_update_phys.py
+++ fv3gfs-physics/fv3gfs/physics/stencils/fv_update_phys.py
@@ -1,28 +1,23 @@
 import gt4py.gtscript as gtscript
 from gt4py.gtscript import (
-    BACKWARD,
     FORWARD,
     PARALLEL,
     computation,
     exp,
-    horizontal,
     interval,
     log,
-    region,
 )
 
-import fv3core.utils.global_config as global_config
 import fv3core.utils.gt4py_utils as utils
 import fv3gfs.util
 import fv3gfs.util as fv3util
 from fv3core.stencils.c2l_ord import CubedToLatLon
 from fv3core.utils.stencil import StencilFactory
-from fv3core.utils.typing import Float, FloatField, FloatFieldI, FloatFieldIJ
+from fv3core.utils.typing import Float, FloatField, FloatFieldIJ
 from fv3gfs.physics.global_constants import *
 
 # TODO: we don't want to import from fv3core
 from fv3gfs.physics.stencils.update_dwind_phys import AGrid2DGridPhysics
-from fv3gfs.util.quantity import Quantity
 
 
 # TODO: This is the same as moist_cv.py in fv3core, should move to integration dir
