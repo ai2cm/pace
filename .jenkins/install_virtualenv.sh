bf5f9de5a68725e7c6d00b28a79500f7514cc25c
Jeremy McGibbon <mcgibbon@uw.edu>
2021-12-09 09:51:21 -0800

    install gt4py before packages that depend on it

--- .jenkins/install_virtualenv.sh
+++ .jenkins/install_virtualenv.sh
@@ -20,10 +20,10 @@ git submodule update --init ${pace_dir}/external/daint_venv
 git submodule update --init ${pace_dir}/external/gt4py
 (cd ${pace_dir}/external/daint_venv && ./install.sh ${virtualenv_path})
 source ${virtualenv_path}/bin/activate
+python3 -m pip install ${PACE_INSTALL_FLAGS} ${pace_dir}/external/gt4py/
 python3 -m pip install ${pace_dir}/pace-util/
 python3 -m pip install $wheel_command -c ${pace_dir}/constraints.txt -r fv3core/requirements/requirements_daint.txt
 python3 -m pip install ${PACE_INSTALL_FLAGS} ${pace_dir}/fv3core/
 python3 -m pip install ${PACE_INSTALL_FLAGS} ${pace_dir}/fv3gfs-physics/
-python3 -m pip install ${PACE_INSTALL_FLAGS} ${pace_dir}/external/gt4py/
 python3 -m pip install ${PACE_INSTALL_FLAGS} ${pace_dir}/stencils/
 deactivate
