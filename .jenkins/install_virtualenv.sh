fea3424080ded734fabca672f17787b872706e19
Jeremy McGibbon <jeremym@allenai.org>
2021-12-10 10:04:31 -0800

    Gt4py is used as a submodule (#56)
    This PR updates pace to use gt4py pinned as a submodule, rather than using GT4PY_VERSION.txt. After this PR, the gt4py caches will use the gt4py sha rather than tag in their GT4PY_VERSION.txt files.

--- .jenkins/install_virtualenv.sh
+++ .jenkins/install_virtualenv.sh
@@ -11,17 +11,16 @@ exitError()
 
 # check a virtualenv path has been provided
 test -n "$1" || exitError 1001 ${virtualenv_path} "must pass an argument"
-wheel_dir=/project/s1053/install/wheeldir
-wheel_command="--find-links=$wheel_dir"
-make update_submodules_venv
-virtualenv_path=$1
 pace_dir=`dirname $0`/../
 echo "pace path is ${pace_dir}"
-if [ -z "${GT4PY_VERSION}" ]; then
-    export GT4PY_VERSION=`cat ${pace_dir}/GT4PY_VERSION.txt`
-fi
+wheel_dir=/project/s1053/install/wheeldir
+wheel_command="--find-links=$wheel_dir"
+virtualenv_path=$1
+git submodule update --init ${pace_dir}/external/daint_venv
+git submodule update --init ${pace_dir}/external/gt4py
 (cd ${pace_dir}/external/daint_venv && ./install.sh ${virtualenv_path})
 source ${virtualenv_path}/bin/activate
+python3 -m pip install ${PACE_INSTALL_FLAGS} ${pace_dir}/external/gt4py/
 python3 -m pip install ${pace_dir}/pace-util/
 python3 -m pip install $wheel_command -c ${pace_dir}/constraints.txt -r fv3core/requirements/requirements_daint.txt
 python3 -m pip install ${PACE_INSTALL_FLAGS} ${pace_dir}/fv3core/
--- fv3core/.jenkins/jenkins.sh
+++ fv3core/.jenkins/jenkins.sh
@@ -72,10 +72,13 @@ echo "PYTHON env ${python_env}"
 # get root directory of where jenkins.sh is sitting
 export jenkins_dir=`dirname $0`
 
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PACE_DIR=$SCRIPT_DIR/../../
 
 if [ -z "${GT4PY_VERSION}" ]; then
-    export GT4PY_VERSION=`cat GT4PY_VERSION.txt`
+    export GT4PY_VERSION=`git submodule status ${PACE_DIR}/external/gt4py | awk '{print $1;}'`
 fi
+
 # If the backend is a GTC backend we fetch the caches
 if [[ $backend != *numpy* ]];then
     . ${jenkins_dir}/actions/fetch_caches.sh $backend $experiment
