73b3908ba8d8a34ceee11b7c424c71b331b8cd04
Elynn Wu <elynn510@gmail.com>
2021-12-10 14:27:24 -0800

    add print statement for fetching caches

--- .jenkins/jenkins.sh
+++ .jenkins/jenkins.sh
@@ -79,6 +79,7 @@ if [ -z "${GT4PY_VERSION}" ]; then
 fi
 # If the backend is a GTC backend we fetch the caches
 if [[ $backend != *numpy* ]];then
+    echo "Fetching for exisintg gt_caches"
     . ${jenkins_dir}/actions/fetch_caches.sh $backend $experiment
 fi
 
