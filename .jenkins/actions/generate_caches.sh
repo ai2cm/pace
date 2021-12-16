a336b02a497d53bb8b18bccea4eeb17cbeeed391
Elynn Wu <elynn510@gmail.com>
2021-12-16 10:21:44 -0800

    fix gt4py version path

--- .jenkins/actions/generate_caches.sh
+++ .jenkins/actions/generate_caches.sh
@@ -37,6 +37,6 @@ find .gt_cache* -type d -name \*_pyext_BUILD -prune -exec \rm -rf {} \;
 find .gt_cache* -type d -name __pycache__ -prune -exec \rm -rf {} \;
 echo "Copying GT4Py cache directories to ${CACHE_DIR}"
 mkdir -p ${CACHE_DIR}
-cp `pwd`/GT4PY_VERSION.txt ${CACHE_DIR}
+cp `pwd`/../GT4PY_VERSION.txt ${CACHE_DIR}
 rm -rf ${CACHE_DIR}/.gt_cache
 cp -rp .gt_cache ${CACHE_DIR}
