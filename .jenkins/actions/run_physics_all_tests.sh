43427e9d93519ce1a9413e27fe1abe7b714c8132
Elynn Wu <elynn510@gmail.com>
2021-12-23 12:08:00 -0800

    update jenkins script

---
+++ .jenkins/actions/run_physics_all_tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e -x
+BACKEND=$1
+EXPNAME=$2
+XML_REPORT="sequential_test_results.xml"
+export TEST_ARGS="-v -s -rsx --backend=${BACKEND} "
+
+JENKINS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/../"
+
+if [ ${python_env} == "virtualenv" ]; then
+    export TEST_ARGS="${TEST_ARGS} --junitxml=${JENKINS_DIR}/${XML_REPORT}"
+    CONTAINER_CMD="srun" make physics_savepoint_tests && make physics_savepoint_tests_mpi
+else
+    export TEST_ARGS="${TEST_ARGS} --junitxml=/${XML_REPORT}"
+    make physics_savepoint_tests && make physics_savepoint_tests_mpi
+fi
--- .jenkins/pace_physics_cache_setup.sh
+++ .jenkins/pace_physics_cache_setup.sh
@@ -2,6 +2,5 @@
 if [[ "${NODE_NAME}" == *"daint"* ]] ; then source ~/.bashrc ; fi
 set -e
 export LONG_EXECUTION=1
-.jenkins/jenkins.sh run_physics_regression_tests ${backend} ${experiment}
-.jenkins/jenkins.sh run_physics_parallel_regression_tests ${backend} ${experiment}
+.jenkins/jenkins.sh run_physics_all_tests ${backend} ${experiment}
 .jenkins/generate_caches.sh ${backend} ${experiment}
