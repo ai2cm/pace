1c2df606981848331aa98f83ddfc1ea5cea2106b
Elynn Wu <elynn510@gmail.com>
2022-01-05 15:20:53 -0800

    fix parallel call

--- .jenkins/actions/run_physics_all_tests.sh
+++ .jenkins/actions/run_physics_all_tests.sh
@@ -9,7 +9,7 @@ JENKINS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd 
 
 if [ ${python_env} == "virtualenv" ]; then
     export TEST_ARGS="${TEST_ARGS} --junitxml=${JENKINS_DIR}/${XML_REPORT}"
-    CONTAINER_CMD="srun" make physics_savepoint_tests && make physics_savepoint_tests_mpi
+    CONTAINER_CMD="srun" make physics_savepoint_tests && CONTAINER_CMD="" make physics_savepoint_tests_mpi make physics_savepoint_tests_mpi
 else
     export TEST_ARGS="${TEST_ARGS} --junitxml=/${XML_REPORT}"
     make physics_savepoint_tests && make physics_savepoint_tests_mpi
