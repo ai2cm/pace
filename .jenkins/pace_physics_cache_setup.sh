54d5a554f7fd0351fe968a7b0a7b1245d647d4c1
Elynn Wu <elynn510@gmail.com>
2022-01-05 22:15:52 -0800

    reset buildenv after serial test

--- .jenkins/pace_physics_cache_setup.sh
+++ .jenkins/pace_physics_cache_setup.sh
@@ -3,5 +3,6 @@ if [[ "${NODE_NAME}" == *"daint"* ]] ; then source ~/.bashrc ; fi
 set -e
 export LONG_EXECUTION=1
 .jenkins/jenkins.sh run_physics_regression_tests ${backend} ${experiment}
+cd buildenv && git reset --hard && cd ..
 .jenkins/jenkins.sh run_physics_parallel_regression_tests ${backend} ${experiment}
 .jenkins/generate_caches.sh ${backend} ${experiment}
