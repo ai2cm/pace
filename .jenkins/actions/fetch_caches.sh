87256a633da0e2fd42536c82233f8d5a1df2c91e
Elynn Wu <elynn510@gmail.com>
2022-01-14 14:29:24 -0800

    use latest cache convention for physics

--- .jenkins/actions/fetch_caches.sh
+++ .jenkins/actions/fetch_caches.sh
@@ -2,23 +2,23 @@
 BACKEND=$1
 EXPNAME=$2
 SANITIZED_BACKEND=`echo $BACKEND | sed 's/:/_/g'` #sanitize the backend from any ':'
-CACHE_DIR="/scratch/snx3000/olifu/jenkins/scratch/store_gt_caches"
+CACHE_DIR="/scratch/snx3000/olifu/jenkins/scratch/store_gt_caches/${EXPNAME}/${SANITIZED_BACKEND}"
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PACE_DIR=$SCRIPT_DIR/../../
+
+if [ -z "${GT4PY_VERSION}" ]; then
+    export GT4PY_VERSION=`git submodule status ${PACE_DIR}/external/gt4py | awk '{print $1;}'`
+fi
+
 if [ ! -d $(pwd)/fv3gfs-physics/.gt_cache ]; then
-    if [ -d ${CACHE_DIR}/${EXPNAME}/${SANITIZED_BACKEND} ]; then
-        version_file=${CACHE_DIR}/${EXPNAME}/${SANITIZED_BACKEND}/GT4PY_VERSION.txt
-        if [ -f ${version_file} ]; then
-            version=`cat ${version_file}`
+    if [ -d ${CACHE_DIR} ]; then
+        cache_filename=${CACHE_DIR}/${GT4PY_VERSION}.tar.gz
+        if [ -f "${cache_filename}" ]; then
+            tar -xf ${cache_filename} -C fv3gfs-physics/.
+            find fv3gfs-physics/. -name m_\*.py -exec sed -i "s|PLACEHOLDER/fv3gfs-physics|$(pwd)/fv3gfs-physics|g" {} +
+            echo ".gt_cache successfully fetched from ${cache_filename}"
         else
-            version=""
-        fi
-        if [ "$version" == "$GT4PY_VERSION" ]; then
-            if [ -d ${CACHE_DIR}/${EXPNAME}/${SANITIZED_BACKEND}/.gt_cache  ]; then
-                cp -r ${CACHE_DIR}/${EXPNAME}/${SANITIZED_BACKEND}/.gt_cache fv3gfs-physics/.
-                find fv3gfs-physics/. -name m_\*.py -exec sed -i "s|\/scratch\/snx3000\/olifu\/jenkins_submit\/workspace\/pace-physics-cache-setup\/backend\/${SANITIZED_BACKEND}\/experiment\/${EXPNAME}\/slave\/daint_submit/fv3gfs-physics|$(pwd)/fv3gfs-physics|g" {} +
-                echo "gt_cache successfully fetched from: ${CACHE_DIR}/${EXPNAME}/${SANITIZED_BACKEND}/.gt_cache"
-            else
-                echo "gt_cache not fetched, caches not found in: ${CACHE_DIR}/${EXPNAME}/${SANITIZED_BACKEND}/.gt_cache"
-            fi
+            echo ".gt_cache not fetched, cache not found at ${cache_filename}"
         fi
     fi
 fi
--- .jenkins/generate_caches.sh
+++ .jenkins/generate_caches.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# Jenkins action to run a benchmark of dynamics.py on Piz Daint
-# 3/11/2021, Tobias Wicky, Vulcan Inc
-
+# Script to generate gt caches on Piz Daint
 # Syntax:
-# .jenkins/action/run_standlone.sh <option>
+# .jenkins/generate_caches.sh <backend> <experiment>
 
-## Arguments:
-# $1: <option> which can be either empty, "profile" or "build_cache"
 
 # stop on all errors and echo commands
-set -e
+set -e -x
 
 # utility function for error handling
 exitError()
@@ -24,19 +20,28 @@ exitError()
 backend=$1
 experiment=$2
 SANITIZED_BACKEND=`echo $backend | sed 's/:/_/g'` #sanitize the backend from any ':'
-CACHE_DIR="/scratch/snx3000/olifu/jenkins/scratch/store_gt_caches/${experiment}/${SANITIZED_BACKEND}"
+CACHE_DIR="/scratch/snx3000/olifu/jenkins/scratch/store_gt_caches/${experiment}/${SANITIZED_BACKEND}/"
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PACE_DIR=$SCRIPT_DIR/../
 
+if [ -z "${GT4PY_VERSION}" ]; then
+    export GT4PY_VERSION=`git submodule status ${PACE_DIR}/external/gt4py | awk '{print $1;}'`
+    echo "GT4PY_VERSION is ${GT4PY_VERSION}"
+fi
+
+CACHE_FILENAME=${CACHE_DIR}/${GT4PY_VERSION}.tar.gz
 
 test -n "${experiment}" || exitError 1001 ${LINENO} "experiment is not defined"
 test -n "${SANITIZED_BACKEND}" || exitError 1002 ${LINENO} "backend is not defined"
-# caches are built inside fv3gfs-physics
+
 cd fv3gfs-physics
+# change path name to PLACEHOLDER for later use
+find fv3gfs-physics/. -name m_\*.py -exec sed -i "s|\/scratch\/snx3000\/olifu\/jenkins_submit\/workspace\/pace-physics-cache-setup\/backend\/${SANITIZED_BACKEND}\/experiment\/${experiment}\/slave\/daint_submit/fv3gfs-physics|PLACEHOLDER/fv3gfs-physics|g" {} +
 # store cache artifacts (and remove caches afterwards)
 echo "Pruning cache to make sure no __pycache__ and *_pyext_BUILD dirs are present"
 find .gt_cache* -type d -name \*_pyext_BUILD -prune -exec \rm -rf {} \;
 find .gt_cache* -type d -name __pycache__ -prune -exec \rm -rf {} \;
 echo "Copying GT4Py cache directories to ${CACHE_DIR}"
 mkdir -p ${CACHE_DIR}
-cp `pwd`/../GT4PY_VERSION.txt ${CACHE_DIR}
-rm -rf ${CACHE_DIR}/.gt_cache
-cp -rp .gt_cache ${CACHE_DIR}
+rm -rf ${CACHE_FILENAME}
+tar -czf ${CACHE_FILENAME} .gt_cache
