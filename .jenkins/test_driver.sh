4cc1f0bb84c637e1aae329eea19a40be893ba276
Jeremy McGibbon <mcgibbon@uw.edu>
2022-01-20 14:00:46 -0800

    fix test_driver.sh to work from any location

--- .jenkins/test_driver.sh
+++ .jenkins/test_driver.sh
@@ -3,4 +3,4 @@
 JENKINS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 FV3GFS_IMAGE="driver_image" make -C ${JENKINS_DIR}/../docker fv3gfs_image
-docker run --rm driver_image make -C /driver test test_mpi
+docker run --rm driver_image make -C ${JENKINS_DIR}/../driver test test_mpi
