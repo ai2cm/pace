4b156770bfb5a0987f3adacd1b0df6e5704938e2
Rhea George <rheag@allenai.org>
2022-03-01 11:54:51 -0800

    compile sequentially

--- fv3core/examples/standalone/benchmarks/run_on_daint.sh
+++ fv3core/examples/standalone/benchmarks/run_on_daint.sh
@@ -122,17 +122,16 @@ echo "    Extra run in nsys: $DO_NSYS_RUN"
 $FV3CORE_DIR/../.jenkins/fetch_caches.sh $backend $EXPNAME
 
 echo "submitting script to do compilation"
-env_vars="export PYTHONOPTIMIZE=TRUE\nexport CRAY_CUDA_MPS=1"
+env_vars="export PYTHONOPTIMIZE=TRUE"
 # Adapt batch script to compile the code:
 sed -i "s/<NAME>/compilestandalone/g" compile.daint.slurm
-sed -i "s/<NTASKS>/$ranks/g" compile.daint.slurm
+sed -i "s/<NTASKS>/1/g" compile.daint.slurm
 sed -i "s/<NTASKSPERNODE>/1/g" compile.daint.slurm
 sed -i "s/<CPUSPERTASK>/$NTHREADS/g" compile.daint.slurm
 sed -i "s/<OUTFILE>/compile.daint.out\n#SBATCH --hint=nomultithread/g" compile.daint.slurm
 sed -i "s/00:45:00/01:40:00/g" compile.daint.slurm
 sed -i "s/cscsci/normal/g" compile.daint.slurm
-sed -i "s/<G2G>/export CRAY_CUDA_MPS=1\nexport PYTHONOPTIMIZE=TRUE/g" compile.daint.slurm
-sed -i "s#<CMD>#$env_vars\nsrun python examples/standalone/runfile/dynamics.py $data_path 1 $backend $githash#g" compile.daint.slurm
+sed -i "s#<CMD>#$env_vars\nsrun python examples/standalone/runfile/compile.py $data_path $backend #g" compile.daint.slurm
 # execute on a gpu node
 set +e
 res=$(sbatch -W -C gpu compile.daint.slurm 2>&1)
---
+++ fv3core/examples/standalone/runfile/compile.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python3
+
+import copy
+import json
+from argparse import ArgumentParser, Namespace
+from datetime import datetime
+from typing import Any, Dict, List, Tuple
+
+import f90nml
+import numpy as np
+import serialbox
+import yaml
+from mpi4py import MPI
+
+import pace.dsl.stencil
+from fv3core._config import DynamicalCoreConfig
+from fv3core.initialization.baroclinic import init_baroclinic_state
+from fv3core.testing import TranslateFVDynamics
+from pace.util.grid import DampingCoefficients, GridData, MetricTerms
+
+
+# Dev note: the GTC toolchain fails if xarray is imported after gt4py
+# pace.util imports xarray if it's available in the env.
+# fv3core imports gt4py.
+# To avoid future conflict creeping back we make util imported prior to
+# fv3core. isort turned off to keep it that way.
+# isort: off
+import pace.util as util
+from fv3core.utils.null_comm import NullComm
+
+# isort: on
+
+import fv3core
+from pace.stencils.testing.grid import Grid
+
+@contextlib.contextmanager
+def no_lagrangian_contributions(dynamical_core: fv3core.DynamicalCore):
+    # TODO: lagrangian contributions currently cause an out of bounds iteration
+    # when halo updates are disabled. Fix that bug and remove this decorator.
+    # Probably requires an update to gt4py (currently v36).
+    def do_nothing(*args, **kwargs):
+        pass
+
+    original_attributes = {}
+    for obj in (
+        dynamical_core._lagrangian_to_eulerian_obj._map_single_delz,
+        dynamical_core._lagrangian_to_eulerian_obj._map_single_pt,
+        dynamical_core._lagrangian_to_eulerian_obj._map_single_u,
+        dynamical_core._lagrangian_to_eulerian_obj._map_single_v,
+        dynamical_core._lagrangian_to_eulerian_obj._map_single_w,
+        dynamical_core._lagrangian_to_eulerian_obj._map_single_delz,
+    ):
+        original_attributes[obj] = obj._lagrangian_contributions
+        obj._lagrangian_contributions = do_nothing  # type: ignore
+    for (
+        obj
+    ) in dynamical_core._lagrangian_to_eulerian_obj._mapn_tracer._list_of_remap_objects:
+        original_attributes[obj] = obj._lagrangian_contributions
+        obj._lagrangian_contributions = do_nothing  # type: ignore
+    try:
+        yield
+    finally:
+        for obj, original in original_attributes.items():
+            obj._lagrangian_contributions = original
+
+def parse_args() -> Namespace:
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        "data_dir",
+        type=str,
+        action="store",
+        help="directory containing data to run with",
+    )
+    parser.add_argument(
+        "backend",
+        type=str,
+        action="store",
+        help="gt4py backend to use",
+    )
+   
+    return parser.parse_args()
+
+
+
+
+def setup_dycore(config, rank, backend) -> Tuple[fv3core.DynamicalCore, List[Any]]:
+    stencil_config = pace.dsl.stencil.StencilConfig(
+        backend=backend,
+        rebuild=False,
+        validate_args=True,
+    )
+   
+    mpi_comm = NullComm(
+        rank=rank, total_ranks=6 * config.layout[0] * config.layout[1], fill_value=0.0
+    )
+    partitioner = pace.util.CubedSpherePartitioner(
+        pace.util.TilePartitioner(config.layout)
+    )
+    communicator = pace.util.CubedSphereCommunicator(mpi_comm, partitioner)
+    sizer = pace.util.SubtileGridSizer.from_tile_params(
+        nx_tile=config.npx - 1,
+        ny_tile=config.npy - 1,
+        nz=config.npz,
+        n_halo=3,
+        extra_dim_lengths={},
+        layout=config.layout,
+        tile_partitioner=partitioner.tile,
+        tile_rank=communicator.tile.rank,
+    )
+    grid_indexing = pace.dsl.stencil.GridIndexing.from_sizer_and_communicator(
+        sizer=sizer, cube=communicator
+    )
+    quantity_factory = pace.util.QuantityFactory.from_backend(
+        sizer=sizer, backend=backend
+    )
+    metric_terms = MetricTerms(
+        quantity_factory=quantity_factory,
+        communicator=communicator,
+    )
+
+    # create an initial state from the Jablonowski & Williamson Baroclinic
+    # test case perturbation. JRMS2006
+    state = baroclinic_init.init_baroclinic_state(
+        metric_terms,
+        adiabatic=config.adiabatic,
+        hydrostatic=config.hydrostatic,
+        moist_phys=config.moist_phys,
+        comm=communicator,
+    )
+    stencil_factory = pace.dsl.stencil.StencilFactory(
+        config=stencil_config,
+        grid_indexing=grid_indexing,
+    )
+
+    dycore = fv3core.DynamicalCore(
+        comm=communicator,
+        grid_data=GridData.new_from_metric_terms(metric_terms),
+        stencil_factory=stencil_factory,
+        damping_coefficients=DampingCoefficients.new_from_metric_terms(metric_terms),
+        config=config,
+        phis=state.phis,
+    )
+    do_adiabatic_init = False
+    # TODO compute from namelist
+    bdt = config.dt_atmos
+
+    args = [
+        state,
+        config.consv_te,
+        do_adiabatic_init,
+        bdt,
+        config.n_split,
+    ]
+    return dycore, args
+
+if __name__ == "__main__":
+    args = parse_args()
+    namelist = f90nml.read(args.data_dir + "/input.nml")
+    dycore_config = DynamicalCoreConfig.from_f90nml(namelist)
+    for rank in range(dycore_config.layout[0] * dycore_config.layout[1]):
+        dycore, args = setup_dycore(dycore_config, rank, args.backend)
+        with no_lagrangian_contributions(dynamical_core=dycore):
+            dycore.step_dynamics(*args)
