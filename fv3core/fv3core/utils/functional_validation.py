dba1cdbc78eb938073e0783380d2188258eb3a90
Jeremy McGibbon <mcgibbon@uw.edu>
2022-03-07 16:42:29 -0800

    merge main_uc and main_vc stencils in fxadv (#129)
    ## Purpose
    
    This PR merges two stencils in fxadv.py to improve performance.
    
    ## Code changes:
    
    - Merged main_uc_stencil and main_vc_stencil in fxadv.py
    - Remove two data copy operations
    - No longer call edge/corner stencils if we aren't on an edge/corner rank in fxadv.py
    - Disable validation of FxAdv on outermost halo for uc_contra and vc_contra

---
+++ fv3core/fv3core/utils/functional_validation.py
@@ -0,0 +1,40 @@
+import copy
+from typing import Callable, Sequence, Tuple
+
+import numpy as np
+
+from pace.dsl.stencil import GridIndexing
+
+
+def get_subset_func(
+    grid_indexing: GridIndexing,
+    dims: Sequence[str],
+    n_halo: Tuple[Tuple[int, int], Tuple[int, int]] = ((0, 0), (0, 0)),
+) -> Callable[[np.ndarray], np.ndarray]:
+    origin, domain = grid_indexing.get_origin_domain(dims)
+    i_start = origin[0] - n_halo[0][0]
+    i_end = origin[0] + domain[0] + n_halo[0][1]
+    j_start = origin[1] - n_halo[1][0]
+    j_end = origin[1] + domain[1] + n_halo[1][1]
+
+    def subset(data):
+        return data[i_start:i_end, j_start:j_end, :]
+
+    return subset
+
+
+def get_set_nan_func(
+    grid_indexing: GridIndexing,
+    dims: Sequence[str],
+    n_halo: Tuple[Tuple[int, int], Tuple[int, int]] = ((0, 0), (0, 0)),
+) -> Callable[[np.ndarray], np.ndarray]:
+    subset = get_subset_func(grid_indexing=grid_indexing, dims=dims, n_halo=n_halo)
+
+    def set_nans(data):
+        safe = copy.deepcopy(data)
+        data[:] = np.nan
+        # data_subset is a view of data, so modifying data_subset modifies data
+        data_subset = subset(data)
+        data_subset[:] = subset(safe)
+
+    return set_nans
