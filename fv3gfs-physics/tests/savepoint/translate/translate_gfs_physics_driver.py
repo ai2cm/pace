aa4f1dd5ff948f046119aae7cf4837cba5b852fd
Rhea George <rheag@allenai.org>
2022-03-13 20:47:18 -0700

    gfs physics driver calls the state updater and the microphysics threshold is adjusted (different dataset)

--- fv3gfs-physics/tests/savepoint/translate/translate_gfs_physics_driver.py
+++ fv3gfs-physics/tests/savepoint/translate/translate_gfs_physics_driver.py
@@ -3,6 +3,7 @@ import copy
 import pace.dsl.gt4py_utils as utils
 import pace.util as util
 from fv3gfs.physics.stencils.physics import Physics, PhysicsState
+from pace.stencils import update_atmos_state
 from pace.stencils.testing.translate_physics import TranslatePhysicsFortranData2Py
 
 
@@ -134,6 +135,10 @@ class TranslateGFSPhysicsDriver(TranslatePhysicsFortranData2Py):
             self.namelist,
             active_packages=active_packages,
         )
+        vars_to_physics = update_atmos_state.DycoreToPhysics(
+            stencil_factory=self.stencil_factory
+        )
+        vars_to_physics(dycore_state=physics_state, physics_state=physics_state)
         physics._atmos_phys_driver_statein(
             physics_state.prsik,
             physics_state.phii,
