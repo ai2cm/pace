7646988ce9b196ca2271177367f2c2d4e52dd5e7
Elynn Wu <elynn510@gmail.com>
2022-03-15 15:34:11 -0700

    test to see if env variables set correctly

--- fv3core/.jenkins/actions/run_regression_tests.sh
+++ fv3core/.jenkins/actions/run_regression_tests.sh
@@ -13,6 +13,12 @@ make get_test_data
 if [ ${python_env} == "virtualenv" ]; then
     export TEST_ARGS="${TEST_ARGS} --junitxml=${JENKINS_DIR}/${XML_REPORT}"
     if [[ ${FV3_DACEMODE} == "True" ]]; then
+        export DACE_execution_general_check_args=0
+        export DACE_frontend_dont_fuse_callbacks=1
+        export DACE_compiler_cpu_openmp_sections=0
+        export DACE_compiler_cuda_max_concurrent_streams=-1
+        export DACE_frontend_unroll_threshold=0
+        export DACE_compiler_unique_functions=none
         CONTAINER_CMD="srun" make savepoint_tests
     else
         CONTAINER_CMD="srun" make tests savepoint_tests
