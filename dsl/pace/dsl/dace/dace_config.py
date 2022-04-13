e4ab96ef9183de2cbf99842e6295ab83f50b21c2
Rhea George <rheag@allenai.org>
2022-04-13 15:29:48 -0700

    remove whitelist, make compilation happen at call-time, disable code generation when using sdfg

--- dsl/pace/dsl/dace/dace_config.py
+++ dsl/pace/dsl/dace/dace_config.py
@@ -33,26 +33,4 @@ class DaceConfig:
         return self.orchestrate
 
 
-# See description below. Dangerous, should be refactored out
-# Either we can JIT properly via GTC or the compute path need to be able
-# to trigger compilation at call time properly if you haven't anyone above you
-def is_dacemode_codegen_whitelisted(func: Callable[..., None]) -> bool:
-    """Whitelist of stencil function that need code generation in DACE mode.
-    Some stencils are called within the __init__ and therefore will need to
-    be pre-compiled nonetheless.
-    """
-    whitelist = [
-        "dp_ref_compute",
-        "cubic_spline_interpolation_constants",
-        "calc_damp",
-        "set_gz",
-        "set_pem",
-        "copy_defn",
-        "compute_geopotential",
-        # DynamicalCore
-        "init_pfull",
-    ]
-    return any(func.__name__ in name for name in whitelist)
-
-
 dace_config = DaceConfig()
