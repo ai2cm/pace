962399edc2e87052b353b46b7d5d5f04386cb47b
Jeremy McGibbon <mcgibbon@uw.edu>
2022-04-14 12:43:01 -0700

    add summary logging to pace.driver.run, and cli options to configure

--- driver/pace/driver/run.py
+++ driver/pace/driver/run.py
@@ -5,7 +5,7 @@ import logging
 import os
 import subprocess
 from datetime import datetime, timedelta
-from typing import Any, Dict, List, Tuple, Union
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 import click
 import dacite
@@ -699,16 +699,73 @@ def _setup_factories(
     return quantity_factory, stencil_factory
 
 
+log_levels = {
+    "info": logging.INFO,
+    "debug": logging.DEBUG,
+    "warning": logging.WARNING,
+    "error": logging.ERROR,
+    "critical": logging.CRITICAL,
+}
+
+
+def configure_logging(log_rank: Optional[int], log_level: str):
+    """
+    Configure logging for the driver.
+
+    Args:
+        log_rank: rank to log from, or 'all' to log to all ranks,
+            forced to 'all' if running without MPI
+        log_level: log level to use
+    """
+    level = log_levels[log_level]
+    if MPI is None:
+        logging.basicConfig(
+            level=level,
+            format="%(asctime)s [%(levelname)s] %(name)s:%(message)s",
+            handlers=[logging.StreamHandler()],
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+    else:
+        if log_rank is None or int(log_rank) == MPI.COMM_WORLD.Get_rank():
+            logging.basicConfig(
+                level=level,
+                format=(
+                    f"%(asctime)s [%(levelname)s] (rank {MPI.COMM_WORLD.Get_rank()}) "
+                    "%(name)s:%(message)s"
+                ),
+                handlers=[logging.StreamHandler()],
+                datefmt="%Y-%m-%d %H:%M:%S",
+            )
+
+
 @click.command()
 @click.argument(
-    "config_path",
+    "CONFIG_PATH",
     required=True,
+    type=click.Path(exists=True, readable=True, dir_okay=False, resolve_path=True),
 )
-def command_line(config_path: str):
+@click.option(
+    "--log-rank",
+    type=click.INT,
+    help="rank to log from, or all ranks by default, ignored if running without MPI",
+)
+@click.option(
+    "--log-level",
+    default="info",
+    help="one of 'info', 'debug', 'warning', 'error', 'critical'",
+)
+def command_line(config_path: str, log_rank: Optional[int], log_level: str):
+    """
+    Run the driver.
+
+    CONFIG_PATH is the path to a DriverConfig yaml file.
+    """
+    configure_logging(log_rank=log_rank, log_level=log_level)
     logger.info("loading DriverConfig from yaml")
     with open(config_path, "r") as f:
         config = yaml.safe_load(f)
         driver_config = DriverConfig.from_dict(config)
+    logging.info(f"DriverConfig loaded: {yaml.dump(dataclasses.asdict(driver_config))}")
     main(driver_config=driver_config)
 
 
@@ -719,21 +776,4 @@ def main(driver_config: DriverConfig):
 
 
 if __name__ == "__main__":
-    if MPI is None:
-        logging.basicConfig(
-            level=logging.INFO,
-            format="%(asctime)s [%(levelname)s] %(name)s:%(message)s",
-            handlers=[logging.StreamHandler()],
-            datefmt="%Y-%m-%d %H:%M:%S",
-        )
-    else:
-        logging.basicConfig(
-            level=logging.INFO,
-            format=(
-                f"%(asctime)s [%(levelname)s] (rank {MPI.COMM_WORLD.Get_rank()}) "
-                "%(name)s:%(message)s"
-            ),
-            handlers=[logging.StreamHandler()],
-            datefmt="%Y-%m-%d %H:%M:%S",
-        )
     command_line()
