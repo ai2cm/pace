c48d203879c7fa975bfb4507098da4db37f3abdc
Florian Deconinck <deconinck.florian@gmail.com>
2022-05-16 12:03:12 -0700

    Lint

--- fv3core/fv3core/stencils/mapn_tracer.py
+++ fv3core/fv3core/stencils/mapn_tracer.py
@@ -8,7 +8,6 @@ from fv3core.stencils.map_single import MapSingle
 from pace.dsl.dace.orchestrate import computepath_method
 from pace.dsl.stencil import StencilFactory
 from pace.dsl.typing import FloatField
-
 from pace.util import Quantity
 
 
