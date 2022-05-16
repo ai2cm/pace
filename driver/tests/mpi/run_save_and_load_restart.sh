e8c0c3848005b174c6890bd4a2d5237be2db240b
Elynn Wu <elynn510@gmail.com>
2022-05-16 10:18:00 -0700

    does this work

--- driver/tests/mpi/run_save_and_load_restart.sh
+++ driver/tests/mpi/run_save_and_load_restart.sh
@@ -6,6 +6,6 @@ cp examples/configs/baroclinic_c12_write_restart.yaml baroclinic_c12_run_two_ste
 sed -i 's/seconds: 225/seconds: 450/' baroclinic_c12_run_two_steps.yaml
 sed -i 's/save_restart: true/save_restart: false/' baroclinic_c12_run_two_steps.yaml
 sed -i 's/path: "output.zarr"/path: "run_two_steps_output.zarr"/' baroclinic_c12_run_two_steps.yaml
-$MPIRUN_CALL python -m pace.driver.run baroclinic_c12_write_restart.yaml
-$MPIRUN_CALL python -m pace.driver.run RESTART/restart.yaml
-$MPIRUN_CALL python -m pace.driver.run baroclinic_c12_run_two_steps.yaml
+mpirun -n 6 --mca btl_vader_single_copy_mechanism none python -m pace.driver.run baroclinic_c12_write_restart.yaml
+mpirun -n 6 --mca btl_vader_single_copy_mechanism none python -m pace.driver.run RESTART/restart.yaml
+mpirun -n 6 --mca btl_vader_single_copy_mechanism none python -m pace.driver.run baroclinic_c12_run_two_steps.yaml
