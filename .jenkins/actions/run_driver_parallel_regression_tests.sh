a0e9a8428e2582ea9b6e008ff014231f14ff4681
Elynn Wu <elynn510@gmail.com>
2022-05-18 12:07:11 -0700

    swtich where target is specified

--- .jenkins/actions/run_driver_parallel_regression_tests.sh
+++ .jenkins/actions/run_driver_parallel_regression_tests.sh
@@ -5,7 +5,7 @@ BACKEND=$1
 export TEST_ARGS="-v -s -rsx --backend=${BACKEND} "
 
 if [ ${python_env} == "virtualenv" ]; then
-    TARGET=driver CONTAINER_CMD="" make driver_savepoint_tests_mpi
+    CONTAINER_CMD="" make driver_savepoint_tests_mpi
 else
-    TARGET=driver make driver_savepoint_tests_mpi
+    make driver_savepoint_tests_mpi
 fi
