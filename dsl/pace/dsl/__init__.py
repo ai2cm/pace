2674425a6d62a51ec4dcc66e87afd7f124753ea1
Johann Dahm <johann.dahm@gmail.com>
2022-05-18 11:25:32 -0700

    Set gt_cache directory at module import time

--- dsl/pace/dsl/__init__.py
+++ dsl/pace/dsl/__init__.py
@@ -1,1 +1,10 @@
+import gt4py.config
+
+from pace.util.mpi import MPI
+
 from .stencil import FrozenStencil, StencilConfig, StencilFactory
+
+
+if MPI is not None:
+    gt4py.config.cache_settings["root_path"] = "."
+    gt4py.config.cache_settings["dir_name"] = f".gt_cache_{MPI.COMM_WORLD.Get_rank()}"
