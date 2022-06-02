2abccd79ad7f50bf1c4ba7b4aae5329233574215
Elynn Wu <elynn510@gmail.com>
2022-06-02 15:27:44 -0700

    clean up plotting script

--- driver/examples/plot_pcolormesh_cube.py
+++ driver/examples/plot_pcolormesh_cube.py
@@ -10,14 +10,7 @@ from fv3viz import pcolormesh_cube
 
 
 def parse_args():
-    usage = "usage: python %(prog)s config_file"
-    parser = ArgumentParser(usage=usage)
-    parser.add_argument(
-        "zarr_output",
-        type=str,
-        action="store",
-        help="which pace zarr output file to use",
-    )
+    parser = ArgumentParser()
     parser.add_argument(
         "experiment",
         type=str,
@@ -35,6 +28,13 @@ def parse_args():
         type=int,
         action="store",
         help="variable z-level to be plotted",
+    )
+    parser.add_argument(
+        "--zarr_output",
+        type=str,
+        action="store",
+        help="when plotting locally, specify pace zarr output path",
+        default=None,
     )
     parser.add_argument(
         "--vmin",
@@ -56,6 +56,11 @@ def parse_args():
         action="store",
         help="npx/npy",
         default=192,
+    )
+    parser.add_argument(
+        "--force_symmetric_colorbar",
+        action="store_true",
+        help="force colorbar to be symmetric around zero",
     )
     parser.add_argument(
         "--diff_init",
@@ -94,15 +99,12 @@ def parse_args():
     return parser.parse_args()
 
 
-def gather_fortran_data_at_klevel(
-    path: str, total_ranks: int, cn: int, var: str, klevel: int
-):
+def gather_fortran_data_at_klevel(path: str, cn: int, var: str, klevel: int):
     """Gather Fortran diagnostics output
     Assuming the fileout has this format: atmos_custom_fine_inst.tile%RANK.nc
     where %RANK is the tile number starting from 1
     Args:
         path: direcotry to Fortran output files
-        total_ranks: total number of ranks (e.g., 6, 54)
         cn: resolution (e.g., 12, 48)
         var: variable name to be extracted
         klevel: index number in the k-axis to be read
@@ -112,91 +114,114 @@ def gather_fortran_data_at_klevel(
             "time"
         ]
     )
-    fortran_data = np.zeros((ts_size, total_ranks, cn, cn))
-    for rank in range(total_ranks):
-        f = xr.open_dataset(
+    total_tiles = 6
+    fortran_data = np.zeros((ts_size, total_tiles, cn, cn))
+    for rank in range(total_tiles):
+        with xr.open_dataset(
             path + "/atmos_custom_fine_inst.tile" + str(rank + 1) + ".nc",
             decode_times=False,
-        )
-        fortran_data[:, rank, :, :] = f[var][:, klevel, :, :]
-        f.close()
-    rotated_fortran_data = np.zeros((ts_size, total_ranks, cn, cn))
-    for t in range(ts_size):
-        temp = fortran_data[t, :, :, :]
-        for rank in range(total_ranks):
-            rotated_fortran_data[t, rank, :, :] = temp[rank, :, :].T
-    return rotated_fortran_data
+        ) as f:
+            for t in range(ts_size):
+                fortran_data[t, rank, :, :] = f[var][t, klevel, :, :].T
+    return fortran_data
 
 
-args = parse_args()
-if args.fortran_data_path is not None:
-    if args.fortran_var is None:
-        raise ValueError(
-            "You must specify the variable name to be subtracted in Fortran data."
-        )
-    fortran = gather_fortran_data_at_klevel(
-        args.zarr_output, 6, args.size, args.fortran_var, args.zlevel
-    )
-ds = xr.open_zarr(
-    store=zarr.DirectoryStore(path="/model/output.zarr"), consolidated=False
-)
-python_lat = ds["lat"].values * 180.0 / np.pi
-python_lon = ds["lon"].values * 180.0 / np.pi
-if args.diff_init:
+if __name__ == "__main__":
+    args = parse_args()
     if args.fortran_data_path is not None:
-        raise ValueError(
-            "You cannot plot the difference from Fortran \
-                when plotting the python difference from the first time step."
+        if args.fortran_var is None:
+            raise ValueError(
+                "You must specify the variable name (fortran_var) \
+                    to be subtracted in Fortran data."
+            )
+        fortran = gather_fortran_data_at_klevel(
+            args.fortran_data_path, args.size, args.fortran_var, args.zlevel
         )
-    python_init = (
-        ds[args.variable][:, :, 0 : args.size, 0 : args.size, :]
-        .isel(time=0, z=args.zlevel)
-        .values
-    )
-for t in range(args.start, args.stop):
-    python = (
-        ds[args.variable][:, :, 0 : args.size, 0 : args.size, :]
-        .isel(time=t, z=args.zlevel)
-        .values
-    )
-    if args.fortran_data_path is not None:
-        plotted_data = python - fortran
-    elif args.diff_init:
-        plotted_data = python - python_init
-    else:
-        plotted_data = python
-    fig, ax = plt.subplots(1, 1, subplot_kw={"projection": ccrs.Robinson()})
-    if args.vmin is not None and args.vmax is not None:
-        h = pcolormesh_cube(
-            python_lat,
-            python_lon,
-            plotted_data,
-            cmap=plt.cm.bwr,
-            ax=ax,
-            vmin=args.vmin,
-            vmax=args.vmax,
+    if args.fortran_var is not None and args.fortran_data_path is None:
+        raise ValueError(
+            "You must specify the path (fortran_data_path) to Fortran data."
+        )
+    if args.zarr_output is not None:
+        ds = xr.open_zarr(
+            store=zarr.DirectoryStore(path=args.zarr_output), consolidated=False
         )
     else:
-        h = pcolormesh_cube(
-            python_lat,
-            python_lon,
-            plotted_data,
-            ax=ax,
+        # when plotting through sarus or docker
+        # we expect pace output mounted at /pace
+        ds = xr.open_zarr(
+            store=zarr.DirectoryStore(path="/pace/output.zarr"), consolidated=False
         )
-    fig.colorbar(h, ax=ax, location="bottom", label=f"{args.variable}")
-    title = args.experiment.replace("_", " ")
-    fig.suptitle(f"{title}: {args.variable}, z={args.zlevel}, timestep={t+1}")
-    ax.annotate(
-        "Generated on " + datetime.now().strftime("%m/%d/%y %H:%M:%S"),
-        xy=(1.0, -0.6),
-        xycoords="axes fraction",
-        ha="right",
-        va="center",
-        fontsize=8,
-    )
-    plt.tight_layout()
-    plt.savefig(
-        f"/work/{args.experiment}_{args.variable}_time_{t:02d}.png",
-        dpi=150,
-    )
-    plt.close()
+    python_lat = ds["lat"].values * 180.0 / np.pi
+    python_lon = ds["lon"].values * 180.0 / np.pi
+    if args.diff_init:
+        if args.fortran_data_path is not None:
+            raise ValueError(
+                "You cannot plot the difference from Fortran \
+                    when plotting the python difference from the first time step."
+            )
+        python_init = (
+            ds[args.variable][:, :, 0 : args.size, 0 : args.size, :]
+            .isel(time=0, z=args.zlevel)
+            .values
+        )
+    for t in range(args.start, args.stop):
+        python = (
+            ds[args.variable][:, :, 0 : args.size, 0 : args.size, :]
+            .isel(time=t, z=args.zlevel)
+            .values
+        )
+        if args.fortran_data_path is not None:
+            plotted_data = python - fortran[t, :]
+        elif args.diff_init:
+            plotted_data = python - python_init
+        else:
+            plotted_data = python
+        fig, ax = plt.subplots(1, 1, subplot_kw={"projection": ccrs.Robinson()})
+        if args.force_symmetric_colorbar:
+            abs_max = np.abs(plotted_data).max()
+            h = pcolormesh_cube(
+                python_lat,
+                python_lon,
+                plotted_data,
+                cmap=plt.cm.bwr,
+                ax=ax,
+                vmin=-abs_max,
+                vmax=abs_max,
+            )
+        elif args.vmin is not None and args.vmax is not None:
+            h = pcolormesh_cube(
+                python_lat,
+                python_lon,
+                plotted_data,
+                ax=ax,
+                vmin=args.vmin,
+                vmax=args.vmax,
+            )
+        else:
+            h = pcolormesh_cube(
+                python_lat,
+                python_lon,
+                plotted_data,
+                ax=ax,
+            )
+        fig.colorbar(h, ax=ax, location="bottom", label=f"{args.variable}")
+        title = args.experiment.replace("_", " ")
+        fig.suptitle(f"{title}: {args.variable}, z={args.zlevel}, timestep={t+1}")
+        ax.annotate(
+            "Generated on " + datetime.now().strftime("%m/%d/%y %H:%M:%S"),
+            xy=(1.0, -0.6),
+            xycoords="axes fraction",
+            ha="right",
+            va="center",
+            fontsize=8,
+        )
+        plt.tight_layout()
+        if args.zarr_output is not None:
+            save_path = ""
+        else:
+            save_path = "/work/"
+        plt.savefig(
+            f"{save_path}{args.experiment}_{args.variable}_time_{t:02d}.png",
+            dpi=150,
+        )
+        plt.close()
