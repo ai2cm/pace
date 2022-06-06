35191918082b43d2feecb66c83737393c03f611c
Johann Dahm <johann.dahm@gmail.com>
2022-06-06 16:57:45 -0700

    Fix typo

--- .jenkins/actions/create_caches.sh
+++ .jenkins/actions/create_caches.sh
@@ -4,4 +4,4 @@ backend=$1
 experiment=$2
 
 data_version=$(cd fv3core && EXPERIMENT=$experiment TARGET=driver make get_test_data | tail -1)
-.jenkins/initialize_driver.py fv3core/test_data/$data_version/$experiment/driver $backend
+.jenkins/initialize_caches.py fv3core/test_data/$data_version/$experiment/driver $backend
