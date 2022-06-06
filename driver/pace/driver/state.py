60207edc403fa691beb3c194ac1bacfa09154f20
Elynn Wu <elynn510@gmail.com>
2022-06-07 01:49:36 +0200

    this might work

--- driver/pace/driver/state.py
+++ driver/pace/driver/state.py
@@ -11,6 +11,12 @@ import pace.util.grid
 from pace.util.grid import DampingCoefficients
 
 
+try:
+    import cupy as cp
+except ImportError:
+    cp = None
+
+
 @dataclasses.dataclass()
 class TendencyState:
     """
@@ -138,6 +144,7 @@ def _overwrite_state_from_restart(
     rank: int,
     state: Union[fv3core.DycoreState, fv3gfs.physics.PhysicsState, TendencyState],
     restart_file_prefix: str,
+    is_gpu_backend: bool,
 ):
     """
     Args:
@@ -152,7 +159,12 @@ def _overwrite_state_from_restart(
     df = xr.open_dataset(path + f"/{restart_file_prefix}_{rank}.nc")
     for _field in fields(type(state)):
         if "units" in _field.metadata.keys():
-            state.__dict__[_field.name].data[:] = df[_field.name].data[:]
+            if is_gpu_backend:
+                state.__dict__[_field.name].data[:] = cp.asarray(
+                    df[_field.name].data[:]
+                )
+            else:
+                state.__dict__[_field.name].data[:] = df[_field.name].data[:]
     return state
 
 
@@ -169,15 +181,24 @@ def _restart_driver_state(
     damping_coefficients = DampingCoefficients.new_from_metric_terms(metric_terms)
     driver_grid_data = pace.util.grid.DriverGridData.new_from_metric_terms(metric_terms)
     dycore_state = fv3core.DycoreState.init_zeros(quantity_factory=quantity_factory)
+    is_gpu_backend = "gpu" in dycore_state.u.metadata.gt4py_backend
     dycore_state = _overwrite_state_from_restart(
-        path, rank, dycore_state, "restart_dycore_state"
+        path,
+        rank,
+        dycore_state,
+        "restart_dycore_state",
+        is_gpu_backend,
     )
     active_packages = ["microphysics"]
     physics_state = fv3gfs.physics.PhysicsState.init_zeros(
         quantity_factory=quantity_factory, active_packages=active_packages
     )
     physics_state = _overwrite_state_from_restart(
-        path, rank, physics_state, "restart_physics_state"
+        path,
+        rank,
+        physics_state,
+        "restart_physics_state",
+        is_gpu_backend,
     )
     physics_state.__post_init__(quantity_factory, active_packages)
     tendency_state = TendencyState.init_zeros(
