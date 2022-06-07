044f8055ac416606d4c78afac46839aa7f715906
Elynn Wu <elynn510@gmail.com>
2022-06-08 01:52:06 +0200

    Revert "Merge branch 'fix_caching' into cache-dir-module"
    This reverts commit f28f6c2905ed93ff02a6f9b25283a038277e45c9, reversing
    changes made to 09343275edf9522121241b735c62e3a33a5219f9.

--- fv3core/examples/standalone/benchmarks/run_on_daint.sh
+++ fv3core/examples/standalone/benchmarks/run_on_daint.sh
@@ -127,8 +127,7 @@ sed -i "s/<NTASKS>/6/g" compile.daint.slurm
 sed -i "s/<NTASKSPERNODE>/1/g" compile.daint.slurm
 sed -i "s/<CPUSPERTASK>/$NTHREADS/g" compile.daint.slurm
 sed -i "s/<OUTFILE>/compile.daint.out\n#SBATCH --hint=nomultithread/g" compile.daint.slurm
-sed -i "s/00:45:00/03:30:00/g" compile.daint.slurm
-sed -i "s/cscsci/normal/g" compile.daint.slurm
+sed -i "s/00:45:00/02:00:00/g" compile.daint.slurm
 sed -i "s#<CMD>#export PYTHONOPTIMIZE=TRUE\nsrun python examples/standalone/runfile/compile.py $data_path $backend #g" compile.daint.slurm
 
 
