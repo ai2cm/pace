e0c71ab3ce9cd3d89f9bfcf3bb82d3c8b7e9469a
Johann Dahm <johann.dahm@gmail.com>
2022-06-16 09:50:39 -0700

    Revert "Remove standalone"
    This reverts commit 2318a857c06d1e25e1d682abae351ce2ace2f553.

---
+++ fv3core/examples/standalone/runfile/acoustics.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python3
+from types import SimpleNamespace
+from typing import Any, Dict, List, Optional, Tuple
+
+import click
+import f90nml
+import serialbox
+import yaml
+from timing import collect_data_and_write_to_file
+
+import pace.dsl
+import pace.util as util
+from fv3core._config import DynamicalCoreConfig
+from fv3core.stencils.dyn_core import AcousticDynamics
+from fv3core.testing import TranslateDynCore
+from pace.stencils.testing.grid import Grid
+from pace.util.null_comm import NullComm
+
+
+try:
+    from mpi4py import MPI
+except ImportError:
+    MPI = None
+
+
+def dycore_config_from_namelist(data_directory: str) -> DynamicalCoreConfig:
+    """
+    Reads the namelist at the given directory and sets
+    the global fv3core config to it
+    """
+    namelist = f90nml.read(data_directory + "/input.nml")
+    dycore_config = DynamicalCoreConfig.from_f90nml(namelist)
+    return dycore_config
+
+
+def initialize_serializer(data_directory: str, rank: int = 0) -> serialbox.Serializer:
+    """Creates a Serializer based on the data-directory and the rank"""
+    return serialbox.Serializer(
+        serialbox.OpenModeKind.Read,
+        data_directory,
+        "Generator_rank" + str(rank),
+    )
+
+
+def read_input_data(
+    grid: Grid,
+    namelist: DynamicalCoreConfig,
+    stencil_factory: pace.dsl.stencil.StencilFactory,
+    serializer: serialbox.Serializer,
+) -> Dict[str, Any]:
+    """Uses the serializer to read the input data from disk"""
+    driver_object = TranslateDynCore([grid], namelist, stencil_factory)
+    savepoint_in = serializer.get_savepoint("DynCore-In")[0]
+    return driver_object.collect_input_data(serializer, savepoint_in)
+
+
+def get_state_from_input(
+    grid: Grid, namelist, stencil_config, input_data: Dict[str, Any]
+) -> Dict[str, SimpleNamespace]:
+    """
+    Transforms the input data from the dictionary of strings
+    to arrays into a state  we can pass in
+
+    Input is a dict of arrays. These are transformed into Storage arrays
+    useable in GT4Py
+
+    This will also take care of reshaping the arrays into same sized
+    fields as required by the acoustics
+    """
+    driver_object = TranslateDynCore([grid], namelist, stencil_config)
+    driver_object._base.make_storage_data_input_vars(input_data)
+
+    inputs = driver_object.inputs
+    for name, properties in inputs.items():
+        grid.quantity_dict_update(
+            input_data, name, dims=properties["dims"], units=properties["units"]
+        )
+
+    statevars = SimpleNamespace(**input_data)
+    return {"state": statevars}
+
+
+def set_up_communicator(
+    disable_halo_exchange: bool,
+    layout: Tuple[int, int],
+) -> Tuple[Optional[MPI.Comm], Optional[util.CubedSphereCommunicator]]:
+    partitioner = util.CubedSpherePartitioner(util.TilePartitioner(layout))
+    if MPI is not None:
+        comm = MPI.COMM_WORLD
+    else:
+        comm = None
+    if not disable_halo_exchange:
+        assert comm is not None
+        cube_comm = util.CubedSphereCommunicator(comm, partitioner)
+    else:
+        cube_comm = util.CubedSphereCommunicator(NullComm(0, 0), partitioner)
+    return comm, cube_comm
+
+
+def get_experiment_name(
+    data_directory: str,
+) -> str:
+    return yaml.safe_load(
+        open(
+            data_directory + "/input.yml",
+            "r",
+        )
+    )["experiment_name"]
+
+
+def initialize_timers() -> Tuple[util.Timer, util.Timer, List, List]:
+    total_timer = util.Timer()
+    total_timer.start("total")
+    timestep_timer = util.Timer()
+    return total_timer, timestep_timer, [], []
+
+
+def read_and_reset_timer(timestep_timer, times_per_step, hits_per_step):
+    times_per_step.append(timestep_timer.times)
+    hits_per_step.append(timestep_timer.hits)
+    timestep_timer.reset()
+    return times_per_step, hits_per_step
+
+
+@click.command()
+@click.argument("data_directory", required=True, nargs=1)
+@click.argument("time_steps", required=False, default="1")
+@click.argument("backend", required=False, default="gt:cpu_ifirst")
+@click.option("--disable_halo_exchange/--no-disable_halo_exchange", default=False)
+@click.option("--print_timings/--no-print_timings", default=True)
+def driver(
+    data_directory: str,
+    time_steps: str,
+    backend: str,
+    disable_halo_exchange: bool,
+    print_timings: bool,
+):
+    total_timer, timestep_timer, times_per_step, hits_per_step = initialize_timers()
+    with total_timer.clock("initialization"):
+        dycore_config = dycore_config_from_namelist(data_directory)
+        serializer = initialize_serializer(data_directory)
+        layout = dycore_config.layout
+        mpi_comm, communicator = set_up_communicator(
+            disable_halo_exchange, layout=layout
+        )
+        grid = Grid.with_data_from_namelist(dycore_config, communicator, backend)
+        stencil_config = pace.dsl.stencil.StencilConfig(
+            backend=backend,
+            rebuild=False,
+            validate_args=True,
+        )
+        stencil_factory = pace.dsl.stencil.StencilFactory(
+            config=stencil_config,
+            grid_indexing=grid.grid_indexing,
+        )
+        input_data = read_input_data(grid, dycore_config, stencil_factory, serializer)
+        experiment_name = get_experiment_name(data_directory)
+        nested = False
+        stretched_grid = False
+        acoustics_object = AcousticDynamics(
+            communicator,
+            stencil_factory,
+            grid.grid_data,
+            grid.damping_coefficients,
+            dycore_config.grid_type,
+            nested,
+            stretched_grid,
+            dycore_config.acoustic_dynamics,
+            input_data["pfull"],
+            input_data["phis"],
+        )
+
+        state = get_state_from_input(grid, dycore_config, stencil_config, input_data)
+
+        # warm-up timestep.
+        # We're intentionally not passing the timer here to exclude
+        # warmup/compilation from the internal timers
+        acoustics_object(**state)
+
+    # we set up a specific timer for each timestep
+    # that is cleared after so we get individual statistics
+    for _ in range(int(time_steps) - 1):
+        # this loop is not required
+        # but make performance numbers comparable with FVDynamics
+        for _ in range(dycore_config.k_split):
+            with timestep_timer.clock("DynCore"):
+                acoustics_object(**state)
+        times_per_step, hits_per_step = read_and_reset_timer(
+            timestep_timer, times_per_step, hits_per_step
+        )
+    total_timer.stop("total")
+    times_per_step, hits_per_step = read_and_reset_timer(
+        total_timer, times_per_step, hits_per_step
+    )
+
+    experiment_info = {
+        "name": "acoustics",
+        "dataset": experiment_name,
+        "timesteps": time_steps,
+        "backend": backend,
+        "halo_update": not disable_halo_exchange,
+        "hash": "",
+    }
+    if print_timings:
+        # Collect times and output statistics in json
+        collect_data_and_write_to_file(
+            mpi_comm, hits_per_step, times_per_step, experiment_info
+        )
+
+
+if __name__ == "__main__":
+    driver()
---
+++ fv3core/examples/standalone/runfile/dynamics.py
@@ -0,0 +1,352 @@
+#!/usr/bin/env python3
+
+import copy
+import json
+from argparse import ArgumentParser, Namespace
+from datetime import datetime
+from typing import Any, Dict, List, Tuple
+
+import f90nml
+import numpy as np
+import serialbox
+import yaml
+from mpi4py import MPI
+
+# NOTE: we need to import dsl.stencil prior to
+# pace.util, otherwise xarray precedes gt4py, causing
+# very strange errors on some systems (e.g. daint)
+import pace.dsl.stencil
+import pace.util as util
+from fv3core import DynamicalCore
+from fv3core._config import DynamicalCoreConfig
+from fv3core.initialization.baroclinic import init_baroclinic_state
+from fv3core.testing import TranslateFVDynamics
+from pace.stencils.testing.grid import Grid
+from pace.util.grid import DampingCoefficients, GridData, MetricTerms
+from pace.util.null_comm import NullComm
+
+
+def parse_args() -> Namespace:
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        "data_dir",
+        type=str,
+        action="store",
+        help="directory containing data to run with",
+    )
+    parser.add_argument(
+        "time_step",
+        type=int,
+        action="store",
+        help="number of timesteps to execute",
+    )
+    parser.add_argument(
+        "backend",
+        type=str,
+        action="store",
+        help="gt4py backend to use",
+    )
+    parser.add_argument(
+        "hash",
+        type=str,
+        action="store",
+        help="git hash to store",
+    )
+    parser.add_argument(
+        "--disable_halo_exchange",
+        action="store_true",
+        help="enable or disable the halo exchange",
+    )
+    parser.add_argument(
+        "--disable_json_dump",
+        action="store_true",
+        help="enable or disable json dump",
+    )
+    parser.add_argument(
+        "--profile",
+        action="store_true",
+        help="enable performance profiling using cProfile",
+    )
+
+    return parser.parse_args()
+
+
+def set_experiment_info(
+    experiment_name: str, time_step: int, backend: str, git_hash: str
+) -> Dict[str, Any]:
+    experiment: Dict[str, Any] = {}
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    experiment["setup"] = {}
+    experiment["setup"]["timestamp"] = dt_string
+    experiment["setup"]["dataset"] = experiment_name
+    experiment["setup"]["timesteps"] = time_step
+    experiment["setup"]["hash"] = git_hash
+    experiment["setup"]["version"] = "python/" + backend
+    experiment["setup"]["format_version"] = 2
+    experiment["times"] = {}
+    return experiment
+
+
+def collect_keys_from_data(times_per_step: List[Dict[str, float]]) -> List[str]:
+    """Collects all the keys in the list of dics and returns a sorted version"""
+    keys = set()
+    for data_point in times_per_step:
+        for k, _ in data_point.items():
+            keys.add(k)
+    sorted_keys = list(keys)
+    sorted_keys.sort()
+    return sorted_keys
+
+
+def gather_timing_data(
+    times_per_step: List[Dict[str, float]],
+    results: Dict[str, Any],
+    comm: MPI.Comm,
+    root: int = 0,
+) -> Dict[str, Any]:
+    """returns an updated version of  the results dictionary owned
+    by the root node to hold data on the substeps as well as the main loop timers"""
+    is_root = comm.Get_rank() == root
+    keys = collect_keys_from_data(times_per_step)
+    data: List[float] = []
+    for timer_name in keys:
+        data.clear()
+        for data_point in times_per_step:
+            if timer_name in data_point:
+                data.append(data_point[timer_name])
+
+        sendbuf = np.array(data)
+        recvbuf = None
+        if is_root:
+            recvbuf = np.array([data] * comm.Get_size())
+        comm.Gather(sendbuf, recvbuf, root=0)
+        if is_root:
+            results["times"][timer_name]["times"] = copy.deepcopy(recvbuf.tolist())
+    return results
+
+
+def write_global_timings(experiment: Dict[str, Any]) -> None:
+    now = datetime.now()
+    filename = now.strftime("%Y-%m-%d-%H-%M-%S")
+    with open(filename + ".json", "w") as outfile:
+        json.dump(experiment, outfile, sort_keys=True, indent=4)
+
+
+def gather_hit_counts(
+    hits_per_step: List[Dict[str, int]], results: Dict[str, Any]
+) -> Dict[str, Any]:
+    """collects the hit count across all timers called in a program execution"""
+    for data_point in hits_per_step:
+        for name, value in data_point.items():
+            if name not in results["times"]:
+                print(name)
+                results["times"][name] = {"hits": value, "times": []}
+            else:
+                results["times"][name]["hits"] += value
+    return results
+
+
+def get_experiment_info(data_directory: str) -> Tuple[str, bool]:
+    config_yml = yaml.safe_load(
+        open(
+            data_directory + "/input.yml",
+            "r",
+        )
+    )
+    is_baroclinic_test_case = False
+    if (
+        "test_case_nml" in config_yml["namelist"].keys()
+        and config_yml["namelist"]["test_case_nml"]["test_case"] == 13
+    ):
+        is_baroclinic_test_case = True
+    print(
+        "Running "
+        + config_yml["experiment_name"]
+        + ", and using the baroclinic test case?: "
+        + str(is_baroclinic_test_case)
+    )
+    return config_yml["experiment_name"], is_baroclinic_test_case
+
+
+def read_serialized_initial_state(rank, grid, namelist, stencil_factory, data_dir):
+    # set up of helper structures
+    serializer = serialbox.Serializer(
+        serialbox.OpenModeKind.Read,
+        data_dir,
+        "Generator_rank" + str(rank),
+    )
+    # create a state from serialized data
+    savepoint_in = serializer.get_savepoint("Driver-In")[0]
+    driver_object = TranslateFVDynamics(grid, namelist, stencil_factory)
+    input_data = driver_object.collect_input_data(serializer, savepoint_in)
+    state = driver_object.state_from_inputs(input_data)
+    return state
+
+
+def collect_data_and_write_to_file(
+    args: Namespace, comm: MPI.Comm, hits_per_step, times_per_step, experiment_name
+) -> None:
+    """
+    collect the gathered data from all the ranks onto rank 0 and write the timing file
+    """
+    is_root = comm.Get_rank() == 0
+    results = None
+    if is_root:
+        print("Gathering Times")
+        results = set_experiment_info(
+            experiment_name, args.time_step, args.backend, args.hash
+        )
+        results = gather_hit_counts(hits_per_step, results)
+
+    results = gather_timing_data(times_per_step, results, comm)
+
+    if is_root:
+        write_global_timings(results)
+
+
+def setup_dycore(
+    dycore_config, mpi_comm, backend, is_baroclinic_test_case, data_dir
+) -> Tuple[DynamicalCore, Dict[str, Any]]:
+    # set up grid-dependent helper structures
+    partitioner = util.CubedSpherePartitioner(
+        util.TilePartitioner(dycore_config.layout)
+    )
+    communicator = util.CubedSphereCommunicator(mpi_comm, partitioner)
+    grid = Grid.from_namelist(dycore_config, mpi_comm.rank, backend)
+    stencil_config = pace.dsl.stencil.StencilConfig(
+        backend=backend,
+        rebuild=False,
+        validate_args=False,
+    )
+    stencil_factory = pace.dsl.stencil.StencilFactory(
+        config=stencil_config,
+        grid_indexing=grid.grid_indexing,
+    )
+    metric_terms = MetricTerms.from_tile_sizing(
+        npx=dycore_config.npx,
+        npy=dycore_config.npy,
+        npz=dycore_config.npz,
+        communicator=communicator,
+        backend=backend,
+    )
+    if is_baroclinic_test_case:
+        # create an initial state from the Jablonowski & Williamson Baroclinic
+        # test case perturbation. JRMS2006
+        state = init_baroclinic_state(
+            metric_terms,
+            adiabatic=dycore_config.adiabatic,
+            hydrostatic=dycore_config.hydrostatic,
+            moist_phys=dycore_config.moist_phys,
+            comm=communicator,
+        )
+    else:
+        state = read_serialized_initial_state(
+            mpi_comm.rank, grid, dycore_config, stencil_factory, data_dir
+        )
+    dycore = DynamicalCore(
+        comm=communicator,
+        grid_data=GridData.new_from_metric_terms(metric_terms),
+        stencil_factory=stencil_factory,
+        damping_coefficients=DampingCoefficients.new_from_metric_terms(metric_terms),
+        config=dycore_config,
+        phis=state.phis,
+    )
+    # TODO include functionality that uses and changes this
+    do_adiabatic_init = False
+    # TODO compute from namelist
+    bdt = 225.0
+    args = {
+        "state": state,
+        "conserve_total_energy": dycore_config.consv_te,
+        "do_adiabatic_init": do_adiabatic_init,
+        "timestep": bdt,
+        "n_split": dycore_config.n_split,
+    }
+    return dycore, args
+
+
+if __name__ == "__main__":
+    timer = util.Timer()
+    timer.start("total")
+    with timer.clock("initialization"):
+        args = parse_args()
+        comm = MPI.COMM_WORLD
+        rank = comm.Get_rank()
+
+        profiler = None
+        if args.profile:
+            import cProfile
+
+            profiler = cProfile.Profile()
+            profiler.disable()
+
+        namelist = f90nml.read(args.data_dir + "/input.nml")
+        dycore_config = DynamicalCoreConfig.from_f90nml(namelist)
+        experiment_name, is_baroclinic_test_case = get_experiment_info(args.data_dir)
+        if args.disable_halo_exchange:
+            mpi_comm = NullComm(MPI.COMM_WORLD.Get_rank(), MPI.COMM_WORLD.Get_size())
+        else:
+            mpi_comm = MPI.COMM_WORLD
+        dycore, dycore_args = setup_dycore(
+            dycore_config,
+            mpi_comm,
+            args.backend,
+            is_baroclinic_test_case,
+            args.data_dir,
+        )
+
+        # warm-up timestep.
+        # We're intentionally not passing the timer here to exclude
+        # warmup/compilation from the internal timers
+        if rank == 0:
+            print("timestep 1")
+        dycore.step_dynamics(**dycore_args)
+
+    if profiler is not None:
+        profiler.enable()
+
+    times_per_step = []
+    hits_per_step = []
+    # we set up a specific timer for each timestep
+    # that is cleared after so we get individual statistics
+    timestep_timer = util.Timer()
+    for i in range(args.time_step - 1):
+        with timestep_timer.clock("mainloop"):
+            if rank == 0:
+                print(f"timestep {i+2}")
+            dycore.step_dynamics(
+                **dycore_args,
+                timer=timestep_timer,
+            )
+        times_per_step.append(timestep_timer.times)
+        hits_per_step.append(timestep_timer.hits)
+        timestep_timer.reset()
+
+    if profiler is not None:
+        profiler.disable()
+
+    timer.stop("total")
+    times_per_step.append(timer.times)
+    hits_per_step.append(timer.hits)
+
+    # output profiling data
+    if profiler is not None:
+        profiler.dump_stats(f"fv3core_{experiment_name}_{args.backend}_{rank}.prof")
+
+    # Timings
+    if not args.disable_json_dump:
+        # Collect times and output statistics in json
+        MPI.COMM_WORLD.Barrier()
+        collect_data_and_write_to_file(
+            args, MPI.COMM_WORLD, hits_per_step, times_per_step, experiment_name
+        )
+    else:
+        # Print a brief summary of timings
+        # Dev Note: we especially do _not_ gather timings here to have a
+        # no-MPI-communication codepath
+        print(f"Rank {rank} done. Total time: {timer.times['total']}.")
+
+    if rank == 0:
+        print("SUCCESS")
