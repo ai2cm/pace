2545b2448b377b55cfc49aac403241b9f1f5d0ff
Johann Dahm <johann.dahm@gmail.com>
2022-06-16 15:29:43 -0700

    Allow existing directories

--- .jenkins/initialize_caches.py
+++ .jenkins/initialize_caches.py
@@ -50,4 +50,4 @@ if __name__ == "__main__":
         initialize_caches(namelist, backend, rank, num_ranks)
 
     for rank in range(1, num_ranks):
-        shutil.copytree(f".gt_cache_{0:06}", f".gt_cache_{rank:06}")
+        shutil.copytree(f".gt_cache_{0:06}", f".gt_cache_{rank:06}", dirs_exist_ok=True)
