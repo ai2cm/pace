4e083f95bd09fcda1dbc9086d5d5166bc384e37a
Johann Dahm <johann.dahm@gmail.com>
2022-06-20 11:49:16 -0700

    Parameterize and respect TEST_DATA_ROOT

--- .jenkins/actions/create_caches.sh
+++ .jenkins/actions/create_caches.sh
@@ -3,6 +3,11 @@
 backend=$1
 experiment=$2
 [[ "$3" != "bypass_wrapper" ]] && parallel_prefix="srun"
+target=driver
 
-data_version=$(cd fv3core && EXPERIMENT=$experiment TARGET=driver make get_test_data | tail -1)
-$parallel_prefix .jenkins/initialize_caches.py fv3core/test_data/$data_version/$experiment/driver $backend
+data_version=$(cd fv3core && EXPERIMENT=$experiment TARGET=$target make get_test_data | tail -1)
+
+# Jenkins sets TEST_DATA_ROOT to a custom path and `make get_test_data` uses that above
+data_path=${TEST_DATA_ROOT:-fv3core/test_data}/$data_version/$experiment/$target
+
+$parallel_prefix .jenkins/initialize_caches.py $data_path $backend
