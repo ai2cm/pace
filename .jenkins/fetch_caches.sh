aeaaf03eccd048a5a2a121f8a2db826f687d8191
Johann Dahm <johann.dahm@gmail.com>
2022-06-23 10:25:15 -0700

    Always overwrite/merge caches

--- .jenkins/fetch_caches.sh
+++ .jenkins/fetch_caches.sh
@@ -9,20 +9,15 @@ pace_dir=$script_dir/../
 
 export gt4py_version=$(git submodule status $pace_dir/external/gt4py | awk '{print $1;}')
 
-if ! compgen -G ./.gt_cache* > /dev/null; then
-    if [ -d $cache_dir ]; then
-        cache_filename=$cache_dir/$gt4py_version.tar.gz
-        if [ -f "$cache_filename" ]; then
-            tar -xzf $cache_filename -C .
-            echo "Caches sucessfully loaded from $cache_filename"
-        else
-            echo "Caches $cache_filename not found"
-            exit 1
-        fi
+if [ -d $cache_dir ]; then
+    cache_filename=$cache_dir/$gt4py_version.tar.gz
+    if [ -f "$cache_filename" ]; then
+        tar -xzf $cache_filename -C .
+        echo "Caches sucessfully loaded from $cache_filename"
     else
-        echo "Cache directory $cache_dir not found"
+        echo "Caches $cache_filename not found"
+        exit 1
     fi
 else
-    echo "WARNING: $(pwd)/.gt_cache* already exists. Will not overwrite directory with caches."
-    exit 1
+    echo "Cache directory $cache_dir not found"
 fi
