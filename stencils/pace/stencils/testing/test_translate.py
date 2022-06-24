5bd3ef415fe8c43ee80bb7a99800e3c30d9c9bb8
Jeremy McGibbon <mcgibbon@uw.edu>
2022-06-24 15:25:04 -0700

    increase threshold for fill values

--- stencils/pace/stencils/testing/test_translate.py
+++ stencils/pace/stencils/testing/test_translate.py
@@ -172,7 +172,7 @@ def perturb(input):
     roundoff = 1e-16
     for data in input.values():
         if isinstance(data, np.ndarray) and data.dtype in (np.float64, np.float32):
-            not_fill_value = data < 1e10
+            not_fill_value = data < 1e30
             # multiply data by roundoff-level error
             data[not_fill_value] *= 1.0 + np.random.uniform(
                 low=-roundoff, high=roundoff, size=data[not_fill_value].shape
