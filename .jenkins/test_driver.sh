933d00a7994138c555e279d709a514dda8e5d525
Jeremy McGibbon <mcgibbon@uw.edu>
2022-07-07 22:53:40 +0000

    use new driver path

--- .jenkins/test_driver.sh
+++ .jenkins/test_driver.sh
@@ -3,4 +3,4 @@
 JENKINS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 PACE_IMAGE="driver_image" make -C ${JENKINS_DIR}/.. build
-docker run --rm driver_image make -C /driver test test_mpi
+docker run --rm driver_image make -C /pace/driver test test_mpi
