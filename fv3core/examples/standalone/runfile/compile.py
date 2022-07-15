9a3720eb384b8ceb0ff791199a9997023e536380
tobias <twicki12@gmail.com>
2022-07-15 14:57:34 -0700

    copy correctly

--- fv3core/examples/standalone/runfile/compile.py
+++ fv3core/examples/standalone/runfile/compile.py
@@ -9,7 +9,7 @@ import f90nml
 import pace.dsl.stencil  # noqa: F401
 from fv3core._config import DynamicalCoreConfig
 from pace.util.null_comm import NullComm
-
+import gt4py.config
 
 local = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, local)
@@ -26,10 +26,7 @@ def parse_args() -> Namespace:
         help="directory containing data to run with",
     )
     parser.add_argument(
-        "backend",
-        type=str,
-        action="store",
-        help="gt4py backend to use",
+        "backend", type=str, action="store", help="gt4py backend to use",
     )
 
     return parser.parse_args()
@@ -46,6 +43,12 @@ if __name__ == "__main__":
             total_ranks=6 * dycore_config.layout[0] * dycore_config.layout[1],
             fill_value=0.0,
         )
+        gt4py.config.cache_settings["root_path"] = os.environ.get(
+            "GT_CACHE_DIR_NAME", "."
+        )
+        gt4py.config.cache_settings["dir_name"] = os.environ.get(
+            "GT_CACHE_ROOT", f".gt_cache_{mpi_comm.Get_rank():06}"
+        )
         dycore, dycore_args, stencil_factory = setup_dycore(
             dycore_config,
             mpi_comm,
@@ -54,6 +57,12 @@ if __name__ == "__main__":
             args.data_dir,
         )
     # NOTE (jdahm): Temporary until driver initialization-based cache is merged
-    for rank in range(1, 6 * dycore_config.layout[0] * dycore_config.layout[1]):
-        shutil.copytree(f".gt_cache_{0:06}", f".gt_cache_{rank:06}", dirs_exist_ok=True)
+    root_path = gt4py.config.cache_settings["root_path"]
+    for rank in range(dycore_config.layout[0] * dycore_config.layout[1]):
+        for tile in range(1, 6):
+            shutil.copytree(
+                f"{root_path}/.gt_cache_{rank:06}",
+                f"{root_path}.gt_cache_{rank*tile:06}",
+                dirs_exist_ok=True,
+            )
     print("SUCCESS")
