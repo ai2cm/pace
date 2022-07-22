f12ee20d827b4d061d0fe5a5186e8742fcfb88fb
tobias <twicki12@gmail.com>
2022-07-22 15:22:53 -0700

    testing

--- tests/main/dsl/test_compilation_config.py
+++ tests/main/dsl/test_compilation_config.py
@@ -1,31 +1,38 @@
 import unittest.mock as mock
+from typing import Tuple
 
 import pytest
 
 from pace.dsl.dace.dace_config import DaceConfig, DaCeOrchestration
 from pace.dsl.stencil import CompilationConfig, RunMode, StencilConfig
 from pace.util.decomposition import determine_rank_is_compiling
+from pace.util.partitioner import CubedSpherePartitioner, Partitioner, TilePartitioner
 
 
-def test_compiling_ranks():
-    part = mock.MagicMock(total_ranks=6)
-    comm = mock.MagicMock(rank=0, partitioner=part)
-    config = CompilationConfig(
-        communicator=comm, use_minimal_caching=True, run_mode=RunMode.Run
-    )
+@pytest.mark.parametrize(
+    "layout, rank, is_compiling",
+    [
+        pytest.param((1, 1), 0, True, id="1x1 layout"),
+        pytest.param((1, 1), 2, False, id="1x1 layout"),
+        pytest.param((2, 2), 1, True, id="2x2 layout"),
+        pytest.param((2, 2), 5, False, id="2x2 layout"),
+        pytest.param((3, 3), 8, True, id="3x3 layout"),
+        pytest.param((3, 3), 25, False, id="3x3 layout"),
+    ],
+)
+def test_compiling_ranks(layout: Tuple[int, int], rank: int, is_compiling: bool):
+    partitioner = CubedSpherePartitioner(TilePartitioner(layout))
+    assert determine_rank_is_compiling(rank, partitioner) == is_compiling
 
-    assert determine_rank_is_compiling(config) == True
 
-    part = mock.MagicMock(total_ranks=24)
-    comm = mock.MagicMock(rank=5, partitioner=part)
-    config = CompilationConfig(
-        communicator=comm, use_minimal_caching=True, run_mode=RunMode.Run
-    )
-    assert determine_rank_is_compiling(config) == False
+def test_assert_on_large_layout():
+    with pytest.raises(RuntimeError):
+        partitioner = CubedSpherePartitioner(TilePartitioner((4, 4)))
+        determine_rank_is_compiling(0, partitioner)
 
 
 def tests_configurations():
-    config = CompilationConfig(use_minimal_caching=True, run_mode=RunMode.Run)
+    config = CompilationConfig()  # use_minimal_caching=True, run_mode=RunMode.Run)
 
 
 # @pytest.mark.parametrize("validate_args", [True, False])
