5e1ac4ed712d185d0b4151928e74e9ec17d06fec
Jeremy McGibbon <mcgibbon@uw.edu>
2022-07-25 19:04:08 +0000

    use correct mpirun flags for gce

--- fv3core/.jenkins/actions/run_parallel_regression_tests.sh
+++ fv3core/.jenkins/actions/run_parallel_regression_tests.sh
@@ -8,13 +8,13 @@ export TEST_ARGS="${EXTRA_TEST_ARGS} -v -s -rsx --backend=${BACKEND} "
 make get_test_data
 
 if [ ${python_env} == "virtualenv" ]; then
-    CONTAINER_CMD="" make savepoint_tests_mpi
+    CONTAINER_CMD="" MPIRUN_ARGS="" make savepoint_tests_mpi
 else
     make savepoint_tests_mpi
 fi
 export TEST_ARGS="${TEST_ARGS} --compute_grid"
 if [ ${python_env} == "virtualenv" ]; then
-    CONTAINER_CMD="" make savepoint_tests_mpi
+    CONTAINER_CMD="" MPIRUN_ARGS="" make savepoint_tests_mpi
 else
     make savepoint_tests_mpi
 fi
