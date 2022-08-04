599e566413064f513c080132b9dadc1774b7da7d
tobias <twicki12@gmail.com>
2022-08-04 11:18:11 -0700

    initial setup for plotting a driver run
    Co-authored-by: Elynn Wu <elynn510@gmail.com>

---
+++ .jenkins/run_baroclinic.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+JENKINS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PACE_DIR=$JENKINS_DIR/../
+export VIRTUALENV=${PACE_DIR}/venv
+${JENKINS_DIR}/install_virtualenv.sh ${VIRTUALENV}
+source ${VIRTUALENV}/bin/activate
+
+BUILDENV_DIR=$PACE_DIR/buildenv
+. ${BUILDENV_DIR}/schedulerTools.sh
+
+cd $PACE_DIR
+
+
+cat << EOF > compile.daint.slurm
+#!/bin/bash
+#SBATCH --constraint=gpu
+#SBATCH --job-name=compile_driver
+#SBATCH --ntasks=9
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --output=compile.out
+#SBATCH --time=01:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --account=s1053
+#SBATCH --partition=normal
+
+
+########################################################
+
+set -x
+export OMP_NUM_THREADS=12
+export GT_CACHE_DIR_NAME=/tmp
+srun python ${PACE_DIR}/driver/examples/compile_driver.py ${JENKINS_DIR}/driver_configs/compile_baroclinic_c576_54ranks.yaml ${PACE_DIR}
+EOF
+
+launch_job compile.daint.slurm 7200
+
+cat << EOF > run.daint.slurm
+#!/bin/bash
+#SBATCH --constraint=gpu
+#SBATCH --job-name=run_pace
+#SBATCH --ntasks=54
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --output=run.out
+#SBATCH --time=01:30:00
+#SBATCH --gres=gpu:1
+#SBATCH --account=s1053
+#SBATCH --partition=normal
+
+
+########################################################
+
+set -x
+export OMP_NUM_THREADS=12
+export GT_CACHE_DIR_NAME=/tmp
+srun python ${PACE_DIR}/driver/examples/compile_driver.py ${JENKINS_DIR}/driver_configs/run_baroclinic_c576_54ranks.yaml ${PACE_DIR}
+EOF
+launch_job run.daint.slurm 15000
+
+module load sarus
+sarus pull elynnwu/pace:latest
+echo "####### generating figures..."
+srun -C gpu --partition=debug --account=s1053 --time=00:30:00 sarus run --mount=type=bind,source=${PACE_DIR},destination=/work elynnwu/pace:latest python /work/driver/examples/plot_pcolormesh_cube.py moist_baroclinic_c576 ua 40 --zarr_output=/work/output.zarr --diff_init --start=1 --stop=4 --force_symmetric_colorbar
+echo "####### figures completed."
---
+++ driver/examples/compile_driver.py
@@ -0,0 +1,97 @@
+import copy
+import math
+import os
+import shutil
+from argparse import ArgumentParser
+
+import gt4py
+import yaml
+from mpi4py import MPI
+
+from pace.driver.comm import NullCommConfig
+from pace.driver.run import Driver, DriverConfig
+from pace.util.null_comm import NullComm
+
+
+def parse_args():
+    usage = "usage: python %(prog)s config_file"
+    parser = ArgumentParser(usage=usage)
+
+    parser.add_argument(
+        "config_file",
+        type=str,
+        action="store",
+        help="which config file to use",
+    )
+    parser.add_argument("target_dir", type=str, action="store", help="")
+    return parser.parse_args()
+
+
+def get_iterations_from_config(driver_config: DriverConfig):
+    sub_tiles = (
+        driver_config.dycore_config.layout[0] * driver_config.dycore_config.layout[1]
+    )
+    iterations = math.ceil(sub_tiles / size)
+    return iterations, sub_tiles
+
+
+def setup_driver_objects(global_rank: int, size: int, driver_config: DriverConfig):
+    iterations, sub_tiles = get_iterations_from_config(driver_config)
+    gt4py.config.cache_settings["root_path"] = os.environ.get("GT_CACHE_DIR_NAME", ".")
+    for iteration in range(iterations):
+        top_tile_rank = global_rank + size * iteration
+        if top_tile_rank < sub_tiles:
+            my_driver_config = copy.deepcopy(driver_config)
+            assert isinstance(my_driver_config.comm_config.config, NullCommConfig)
+            my_driver_config.comm_config.config.rank = top_tile_rank
+            gt4py.config.cache_settings["dir_name"] = os.environ.get(
+                "GT_CACHE_ROOT", f".gt_cache_{top_tile_rank:06}"
+            )
+            driver = Driver(
+                config=my_driver_config,
+            )
+    if comm is not None:
+        comm.Barrier()
+
+
+def copy_caches_to_target_location(
+    driver_config: DriverConfig, global_rank: int, size: int, target_dir: str
+):
+    iterations, sub_tiles = get_iterations_from_config(driver_config)
+
+    root_path = gt4py.config.cache_settings["root_path"]
+    for iter in range(iterations):
+        top_tile_rank = global_rank + size * iter
+        if top_tile_rank < sub_tiles:
+            for tile in range(6):
+                shutil.copytree(
+                    f"{root_path}/.gt_cache_{top_tile_rank:06}",
+                    f"{target_dir}/.gt_cache" f"_{(top_tile_rank + tile*sub_tiles):06}",
+                    dirs_exist_ok=True,
+                )
+                print(
+                    f"rank {global_rank} copied for "
+                    f"target rank {(top_tile_rank + tile*sub_tiles)}"
+                )
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    if MPI is not None:
+        comm = MPI.COMM_WORLD
+        global_rank = comm.Get_rank()
+        size = comm.Get_size()
+    else:
+        comm = None
+        global_rank = 0
+        size = 1
+    with open(args.config_file, "r") as f:
+        driver_config = DriverConfig.from_dict(yaml.safe_load(f))
+    setup_driver_objects(global_rank, size, driver_config)
+    copy_caches_to_target_location(driver_config, global_rank, size, args.target_dir)
+
+    if comm is not None:
+        comm.Barrier()
+    print(f"rank {global_rank} is finished, exiting")
+    if global_rank == 0:
+        print("SUCCESS")
