fda689fd88c1dd6d47a7a35241b9ebe4b57936d5
Elynn Wu <elynn510@gmail.com>
2022-08-17 13:12:33 -0700

    change to item

--- pace-util/pace/util/grid/generation.py
+++ pace-util/pace/util/grid/generation.py
@@ -2232,7 +2232,7 @@ class MetricTerms:
         max_area = self._np.max(self.area.data[3:-4, 3:-4])[()]
         min_area_c = self._np.min(self.area_c.data[3:-4, 3:-4])[()]
         max_area_c = self._np.max(self.area_c.data[3:-4, 3:-4])[()]
-        self._da_min = self._comm.comm.allreduce(min_area, min)[0]
-        self._da_max = self._comm.comm.allreduce(max_area, max)[0]
-        self._da_min_c = self._comm.comm.allreduce(min_area_c, min)[0]
-        self._da_max_c = self._comm.comm.allreduce(max_area_c, max)[0]
+        self._da_min = self._comm.comm.allreduce(min_area, min).item()
+        self._da_max = self._comm.comm.allreduce(max_area, max).item()
+        self._da_min_c = self._comm.comm.allreduce(min_area_c, min).item()
+        self._da_max_c = self._comm.comm.allreduce(max_area_c, max).item()
