46d340c77e3616d864de1ec35ca5f43c328815af
Elynn Wu <elynn510@gmail.com>
2022-08-19 10:19:35 -0700

    change to normal queue

--- .jenkins/run_compare_fortran.sh
+++ .jenkins/run_compare_fortran.sh
@@ -39,7 +39,7 @@ cat << EOF > run.daint.slurm
 #SBATCH --time=00:30:00
 #SBATCH --gres=gpu:1
 #SBATCH --account=s1053
-#SBATCH --partition=debug
+#SBATCH --partition=normal
 ########################################################
 set -x
 export OMP_NUM_THREADS=12
@@ -55,4 +55,4 @@ cp -r /project/s1053/fortran_output/wrapper_output/c48_6ranks_baroclinic referen
 
 module load sarus
 sarus pull elynnwu/pace:latest
-srun -C gpu --partition=debug --account=s1053 --time=00:30:00 sarus run --mount=type=bind,source=${PACE_DIR},destination=/work elynnwu/pace:latest python /work/driver/examples/plot_pcolormesh_cube.py dry_baroclinic_c48_comparison ua 40 --start=0 --stop=20 --zarr_output=/work/output.zarr --fortran_data_path=/work/reference_data/c48_6ranks_baroclinic --fortran_var=eastward_wind --fortran_from_wrapper --size=48 --force_symmetric_colorbar
+srun -C gpu --partition=normal --account=s1053 --time=00:30:00 sarus run --mount=type=bind,source=${PACE_DIR},destination=/work elynnwu/pace:latest python /work/driver/examples/plot_pcolormesh_cube.py dry_baroclinic_c48_comparison ua 40 --start=0 --stop=20 --zarr_output=/work/output.zarr --fortran_data_path=/work/reference_data/c48_6ranks_baroclinic --fortran_var=eastward_wind --fortran_from_wrapper --size=48 --force_symmetric_colorbar
