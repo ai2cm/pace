e5ac2c7d90bb04c2198ca35b74a6d464b655f574
tobias <twicki12@gmail.com>
2022-08-23 01:45:39 -0700

    added testing

--- dsl/pace/dsl/stencil_config.py
+++ dsl/pace/dsl/stencil_config.py
@@ -8,12 +8,9 @@ from gtc.passes.oir_pipeline import DefaultPipeline, OirPipeline
 
 from pace.dsl.dace.dace_config import DaceConfig, DaCeOrchestration
 from pace.dsl.gt4py_utils import is_gpu_backend
+from pace.util import TilePartitioner
 from pace.util.communicator import CubedSphereCommunicator
-from pace.util.decomposition import (
-    compiling_equivalent,
-    determine_rank_is_compiling,
-    set_distributed_caches,
-)
+from pace.util.decomposition import determine_rank_is_compiling, set_distributed_caches
 
 
 class RunMode(enum.Enum):
@@ -75,6 +72,49 @@ class CompilationConfig:
                 "Trying to run with a non-square layout is not supported"
             )
 
+    def determine_compiling_equivalent(self, rank: int, partitioner: TilePartitioner):
+        """From my rank & the current partitioner we determine which
+        rank we should read from"""
+        if self.run_mode == RunMode.Run:
+            if partitioner.layout == (1, 1):
+                return 0
+            if partitioner.layout == (2, 2):
+                if partitioner.tile.on_tile_bottom(rank):
+                    if partitioner.tile.on_tile_left(rank):
+                        return 0  # "00"
+                    if partitioner.tile.on_tile_right(rank):
+                        return 1  # "10"
+                if partitioner.tile.on_tile_top(rank):
+                    if partitioner.tile.on_tile_left(rank):
+                        return 2  # "01"
+                    if partitioner.tile.on_tile_right(rank):
+                        return 3  # "11"
+            else:
+                if partitioner.tile.on_tile_bottom(rank):
+                    if partitioner.tile.on_tile_left(rank):
+                        return 0  # "00"
+                    if partitioner.tile.on_tile_right(rank):
+                        return 2  # "20"
+                    else:
+                        return 1  # "10"
+                if partitioner.tile.on_tile_top(rank):
+                    if partitioner.tile.on_tile_left(rank):
+                        return 6  # "02"
+                    if partitioner.tile.on_tile_right(rank):
+                        return 8  # "22"
+                    else:
+                        return 7  # "12"
+                else:
+                    if partitioner.tile.on_tile_left(rank):
+                        return 3  # "01"
+                    if partitioner.tile.on_tile_right(rank):
+                        return 5  # "21"
+                    else:
+                        return 4  # "11"
+        else:
+            tile_size = partitioner.total_ranks / 6
+            return rank % tile_size
+
     def get_decomposition_info_from_comm(
         self, communicator: Optional[CubedSphereCommunicator]
     ) -> Tuple[int, int, int, bool]:
@@ -83,7 +123,7 @@ class CompilationConfig:
             rank = communicator.rank
             size = communicator.partitioner.total_ranks
             if self.use_minimal_caching:
-                equivalent_compiling_rank = compiling_equivalent(
+                equivalent_compiling_rank = self.determine_compiling_equivalent(
                     rank, communicator.partitioner
                 )
                 is_compiling = determine_rank_is_compiling(rank, size)
