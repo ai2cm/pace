c033358e22bf25fbbfd0490417fd025135a1675d
tobias <twicki12@gmail.com>
2022-08-23 15:08:52 -0700

    cleanup

--- .jenkins/run_compare_fortran.sh
+++ .jenkins/run_compare_fortran.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+## Summary:
+# Jenkins plan (only working on Piz daint) to run compare a run of pace with the fortran reference.
+# This will install a virtualenv - make sure submodules are up to date
+
+## Syntax:
+# .jenkins/action/run_compare_fortran.sh <GridType> <InitType>
+
+## Arguments:
+# GridType: OneOf: serialbox, pace
+# InitType: OneOf: serialbox, pace
+
 GRID=$1
 INIT=$2
 JENKINS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
@@ -55,4 +67,4 @@ cp -r /project/s1053/fortran_output/wrapper_output/c48_6ranks_baroclinic referen
 
 module load sarus
 sarus pull elynnwu/pace:latest
-srun -C gpu --partition=normal --account=s1053 --time=00:30:00 sarus run --mount=type=bind,source=${PACE_DIR},destination=/work elynnwu/pace:latest python /work/driver/examples/plot_pcolormesh_cube.py dry_baroclinic_c48_comparison ua 40 --start=0 --stop=20 --zarr_output=/work/output.zarr --fortran_data_path=/work/reference_data/c48_6ranks_baroclinic --fortran_var=eastward_wind --fortran_from_wrapper --size=48 --force_symmetric_colorbar
+srun -C gpu --partition=normal --account=s1053 --time=00:30:00 sarus run --mount=type=bind,source=${PACE_DIR},destination=/work elynnwu/pace:latest python /work/driver/examples/plot_pcolormesh_cube.py dry_baro_c48_FtnRef_G_${GRID:0:1}_I_${INIT:0:1} ua 40 --start=0 --stop=20 --zarr_output=/work/output.zarr --fortran_data_path=/work/reference_data/c48_6ranks_baroclinic --fortran_var=eastward_wind --fortran_from_wrapper --size=48 --force_symmetric_colorbar
