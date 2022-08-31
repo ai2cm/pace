17ebd208b34027836dd71e0989ab7f029406a144
Jeremy McGibbon <mcgibbon@uw.edu>
2022-08-31 18:22:10 +0000

    install wheel

--- driver/examples/create_venv.sh
+++ driver/examples/create_venv.sh
@@ -6,14 +6,13 @@ set -e -x
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-pip3 install --upgrade setuptools wheel
-
 python3 -m venv venv
 . venv/bin/activate
 
 rundir=$(pwd)
 cd ${SCRIPT_DIR}/../../
 
+pip3 install --upgrade setuptools wheel
 pip3 install -r requirements_dev.txt -c constraints.txt
 
 deactivate
