46972703734b92766f9a5b44718351a472f377a8
Johann Dahm <johann.dahm@gmail.com>
2022-09-12 15:33:25 -0700

    Hack in compiler flag to ignore warning

--- fv3core/.jenkins/actions/run_parallel_regression_tests.sh
+++ fv3core/.jenkins/actions/run_parallel_regression_tests.sh
@@ -7,6 +7,8 @@ export TEST_ARGS="${EXTRA_TEST_ARGS} -v -s -rsx --backend=${BACKEND} "
 # sync the test data
 make get_test_data
 
+export CPPFLAGS="${CPPFLAGS} -Wno-unused-but-set-variable"
+
 if [ ${python_env} == "virtualenv" ]; then
     CONTAINER_CMD="" MPIRUN_ARGS="" DEV=n make savepoint_tests_mpi
     TARGET=init CONTAINER_CMD="" MPIRUN_ARGS="" DEV=n make savepoint_tests_mpi
