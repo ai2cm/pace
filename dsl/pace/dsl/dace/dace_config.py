02959fb885774277f85f2700e29d769c7acbe664
Florian Deconinck <deconinck.florian@gmail.com>
2022-09-22 16:04:56 +0200

    syncdebug=False

--- dsl/pace/dsl/dace/dace_config.py
+++ dsl/pace/dsl/dace/dace_config.py
@@ -126,7 +126,7 @@ class DaceConfig:
             )
 
             # Enable to debug GPU failures
-            dace.config.Config.set("compiler", "cuda", "syncdebug", value=True)
+            dace.config.Config.set("compiler", "cuda", "syncdebug", value=False)
 
         # attempt to kill the dace.conf to avoid confusion
         if dace.config.Config._cfg_filename:
