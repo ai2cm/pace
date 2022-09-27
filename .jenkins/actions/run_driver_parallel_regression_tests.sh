a6061c63ccef31dd6c4afd2dafba15f0d82810b3
Elynn Wu <elynn510@gmail.com>
2022-09-27 18:51:25 +0200

    remove duplicate

--- .jenkins/actions/run_driver_parallel_regression_tests.sh
+++ .jenkins/actions/run_driver_parallel_regression_tests.sh
@@ -7,7 +7,7 @@ export TEST_ARGS="-v -s -rsx --backend=${BACKEND} "
 export CPPFLAGS="${CPPFLAGS} -Wno-unused-but-set-variable"
 
 if [ ${python_env} == "virtualenv" ]; then
-    MPIRUN_CALL="srun" CONTAINER_CMD="" MPIRUN_ARGS="" DEV=n make driver_savepoint_tests_mpi
+    CONTAINER_CMD="" MPIRUN_ARGS="" DEV=n make driver_savepoint_tests_mpi
 else
     DEV=n make driver_savepoint_tests_mpi
 fi
--- .jenkins/actions/run_physics_parallel_regression_tests.sh
+++ .jenkins/actions/run_physics_parallel_regression_tests.sh
@@ -4,7 +4,7 @@ BACKEND=$1
 export TEST_ARGS="-v -s -rsx --backend=${BACKEND} "
 
 if [ ${python_env} == "virtualenv" ]; then
-    MPIRUN_CALL="srun" CONTAINER_CMD="" MPIRUN_ARGS="" DEV=n make physics_savepoint_tests_mpi
+    CONTAINER_CMD="" MPIRUN_ARGS="" DEV=n make physics_savepoint_tests_mpi
 else
     DEV=n make physics_savepoint_tests_mpi
 fi
