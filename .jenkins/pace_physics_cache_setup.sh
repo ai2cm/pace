7a5ee22a8e606deb1d62b467941080a9e812b9c5
Elynn Wu <elynn510@gmail.com>
2022-09-27 07:29:23 +0200

    remove redundant fetch caches

--- .jenkins/pace_physics_cache_setup.sh
+++ .jenkins/pace_physics_cache_setup.sh
@@ -4,9 +4,6 @@ PACE_DIR=$SCRIPT_DIR/../
 if [[ "${NODE_NAME}" == *"daint"* ]] ; then source ~/.bashrc ; fi
 set -e
 export LONG_EXECUTION=1
-(cd ${PACE_DIR}/physics && ${PACE_DIR}/.jenkins/fetch_caches.sh $backend $experiment physics)
-cd ${PACE_DIR}
-${PACE_DIR}/.jenkins/fetch_caches.sh $backend $experiment driver
 .jenkins/jenkins.sh run_physics_regression_tests ${backend} ${experiment}
 cd buildenv && git reset --hard && cd ..
 .jenkins/jenkins.sh run_physics_parallel_regression_tests ${backend} ${experiment}
