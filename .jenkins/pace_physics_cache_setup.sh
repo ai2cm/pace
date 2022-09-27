87197c24a87c4d4fd2824f76ad8649a1bf7f812b
Elynn Wu <elynn510@gmail.com>
2022-09-27 20:34:00 +0200

    reset git without chaning dir

--- .jenkins/pace_physics_cache_setup.sh
+++ .jenkins/pace_physics_cache_setup.sh
@@ -5,9 +5,9 @@ if [[ "${NODE_NAME}" == *"daint"* ]] ; then source ~/.bashrc ; fi
 set -e
 export LONG_EXECUTION=1
 .jenkins/jenkins.sh run_physics_regression_tests ${backend} ${experiment}
-cd buildenv && git reset --hard && cd ..
+git -C buildenv reset --hard HEAD
 .jenkins/jenkins.sh run_physics_parallel_regression_tests ${backend} ${experiment}
-cd buildenv && git reset --hard && cd ..
+git -C buildenv reset --hard HEAD
 .jenkins/jenkins.sh run_driver_parallel_regression_tests ${backend} ${experiment}
 cd ${PACE_DIR}/physics
 ${PACE_DIR}/.jenkins/generate_caches.sh ${backend} ${experiment} physics
