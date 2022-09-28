f6a31868e18eb4dd1c36991a0652476bae26c45f
Elynn Wu <elynn510@gmail.com>
2022-09-28 14:49:09 -0700

    Fix driver caching on daint (#348)
    * fix driver caching
    
    * remove redundant fetch caches
    
    * remove duplicate
    
    * reset git without chaning dir

--- .jenkins/pace_physics_cache_setup.sh
+++ .jenkins/pace_physics_cache_setup.sh
@@ -5,7 +5,11 @@ if [[ "${NODE_NAME}" == *"daint"* ]] ; then source ~/.bashrc ; fi
 set -e
 export LONG_EXECUTION=1
 .jenkins/jenkins.sh run_physics_regression_tests ${backend} ${experiment}
-cd buildenv && git reset --hard && cd ..
+git -C buildenv reset --hard HEAD
 .jenkins/jenkins.sh run_physics_parallel_regression_tests ${backend} ${experiment}
+git -C buildenv reset --hard HEAD
+.jenkins/jenkins.sh run_driver_parallel_regression_tests ${backend} ${experiment}
 cd ${PACE_DIR}/physics
 ${PACE_DIR}/.jenkins/generate_caches.sh ${backend} ${experiment} physics
+cd ${PACE_DIR}
+${PACE_DIR}/.jenkins/generate_caches.sh ${backend} ${experiment} driver
