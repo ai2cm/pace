a8b53e35aaedf7ed875a1f38c0e8ece8032a21e4
elynn <elynn510@gmail.com>
2022-10-21 12:48:49 -0700

    cleanup profiler

--- driver/pace/driver/performance/config.py
+++ driver/pace/driver/performance/config.py
@@ -8,10 +8,13 @@ from .collector import (
     PerformanceCollector,
 )
 
+from pace.util import Profiler, NullProfiler
+
 
 @dataclasses.dataclass
 class PerformanceConfig:
     collect_performance: bool = False
+    collect_cProfile: bool = False
     experiment_name: str = "test"
 
     def build(self, comm: pace.util.Comm) -> AbstractPerformanceCollector:
@@ -19,3 +22,8 @@ class PerformanceConfig:
             return PerformanceCollector(experiment_name=self.experiment_name, comm=comm)
         else:
             return NullPerformanceCollector()
+    def build_profiler(self):
+        if self.collect_cProfile:
+            return Profiler()
+        else:
+            return NullProfiler
