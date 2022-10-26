fix calculation of pfull in fv_dynamics

fv3core/pace/fv3core/stencils/fv_dynamics.py
@@ -61,24 +61,6 @@ def set_omega(delp: FloatField, delz: FloatField, w: FloatField, omga: FloatFiel
         omga = delp / delz * w
 
 
-def get_pfull(
-    ak: pace.util.Quantity,
-    bk: pace.util.Quantity,
-    quantity_factory: pace.util.QuantityFactory,
-    p_ref: float,
-) -> pace.util.Quantity:
-    p_interface = ak.view[:] + bk.view[:] * p_ref
-    # pfull = (ph2 - ph1) / log(ph2 / ph1)
-    p_full = quantity_factory.zeros(
-        dims=[pace.util.Z_DIM],
-        units="Pa",
-    )
-    p_full.view[:] = (p_interface[1:] - p_interface[:-1]) / p_full.np.log(
-        p_interface[1:] / p_interface[:-1]
-    )
-    return p_full
-
-
 def fvdyn_temporaries(
     quantity_factory: pace.util.QuantityFactory,
 ) -> Mapping[str, Quantity]:
@@ -265,12 +247,7 @@ class DynamicalCore:
         self._bk = grid_data.bk
         self._phis = phis
         self._ptop = self.grid_data.ptop
-        self._pfull = get_pfull(
-            ak=self._ak,
-            bk=self._bk,
-            quantity_factory=quantity_factory,
-            p_ref=self.config.p_ref,
-        )
+        self._pfull = grid_data.p
         self._fv_setup_stencil = stencil_factory.from_origin_domain(
             moist_cv.fv_setup,
             externals={

util/pace/util/grid/helper.py
@@ -4,6 +4,7 @@ import pathlib
 import xarray as xr
 
 import pace.util
+from pace.util.constants import Z_INTERFACE_DIM
 
 
 # TODO: if we can remove translate tests in favor of checkpointer tests,
@@ -139,6 +140,8 @@ class VerticalGridData:
 
     def __post_init__(self):
         self._dp_ref = None
+        self._p = None
+        self._p_interface = None
 
     @classmethod
     def new_from_metric_terms(cls, metric_terms: MetricTerms) -> "VerticalGridData":
@@ -177,7 +180,35 @@ class VerticalGridData:
         return 1e5
 
     @property
-    def dp_ref(self) -> pace.util.Quantity:
+    def p_interface(self) -> pace.util.Quantity:
+        if self._p_interface is None:
+            p_interface_data = self.ak.view[:] + self.bk.view[:] * self.p_ref
+            self._p_interface = pace.util.Quantity(
+                p_interface_data,
+                dims=[Z_INTERFACE_DIM],
+                units="Pa",
+                gt4py_backend=self.ak.gt4py_backend,
+            )
+        return self._p_interface
+
+    @property
+    def p(self) -> pace.util.Quantity:
+        if self._p is None:
+            p_data = (
+                self.p_interface.view[1:] - self.p_interface.view[:-1]
+            ) / self.p_interface.np.log(
+                self.p_interface.view[1:] / self.p_interface.view[:-1]
+            )
+            self._p = pace.util.Quantity(
+                p_data,
+                dims=[Z_DIM],
+                units="Pa",
+                gt4py_backend=self.p_interface.gt4py_backend,
+            )
+        return self._p
+
+    @property
+    def dp(self) -> pace.util.Quantity:
         if self._dp_ref is None:
             dp_ref_data = (
                 self.ak.view[1:]
@@ -478,6 +509,13 @@ class GridData:
         return self._vertical_data.p_ref
 
     @property
+    def p(self) -> pace.util.Quantity:
+        """
+        Reference pressure profile for Eulerian grid, defined at cell centers.
+        """
+        return self._vertical_data.p
+
+    @property
     def ak(self):
         """
         constant used to define pressure at vertical interfaces,
@@ -512,7 +550,7 @@ class GridData:
 
     @property
     def dp_ref(self) -> pace.util.Quantity:
-        return self._vertical_data.dp_ref
+        return self._vertical_data.dp
 
     @property
     def cosa(self):