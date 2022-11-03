7cbe8f658c94b002bda4b7c4359a1451aa788586
tobias <twicki12@gmail.com>
2022-11-03 20:07:44 +0100

    fast test

--- .jenkins/run_baroclinic.sh
+++ .jenkins/run_baroclinic.sh
@@ -9,18 +9,15 @@ BUILDENV_DIR=$PACE_DIR/buildenv
 . ${BUILDENV_DIR}/schedulerTools.sh
 
 cd $PACE_DIR
-${JENKINS_DIR}/fetch_caches.sh gt:gpu c576_54ranks_minimal_baroclinic driver
-
-
 cat << EOF > compile.daint.slurm
 #!/bin/bash
 #SBATCH --constraint=gpu
 #SBATCH --job-name=compile_driver
-#SBATCH --ntasks=9
+#SBATCH --ntasks=54
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --output=compile.out
-#SBATCH --time=09:00:00
+#SBATCH --time=00:45:00
 #SBATCH --gres=gpu:1
 #SBATCH --account=s1053
 #SBATCH --partition=normal
@@ -30,23 +27,21 @@ cat << EOF > compile.daint.slurm
 
 set -x
 export OMP_NUM_THREADS=12
-export GT_CACHE_DIR_NAME=/tmp
-mv .gt_cache* /tmp/
-srun python ${PACE_DIR}/driver/examples/compile_driver.py ${JENKINS_DIR}/driver_configs/compile_baroclinic_c576_54ranks.yaml ${PACE_DIR}
+export FV3_DACEMODE=Build
+srun python -m pace.driver.run ${JENKINS_DIR}/driver_configs/baroclinic_c576_54ranks.yaml
 EOF
 
 launch_job compile.daint.slurm 45000
-${JENKINS_DIR}/generate_caches.sh gt:gpu c576_54ranks_minimal_baroclinic driver
 
 cat << EOF > run.daint.slurm
 #!/bin/bash
 #SBATCH --constraint=gpu
 #SBATCH --job-name=run_pace
-#SBATCH --ntasks=54
+#SBATCH --ntasks=96
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --output=run.out
-#SBATCH --time=01:30:00
+#SBATCH --time=00:25:00
 #SBATCH --gres=gpu:1
 #SBATCH --account=s1053
 #SBATCH --partition=normal
@@ -56,13 +51,11 @@ cat << EOF > run.daint.slurm
 
 set -x
 export OMP_NUM_THREADS=12
-srun python -m pace.driver.run ${JENKINS_DIR}/driver_configs/run_baroclinic_c576_54ranks.yaml
+export FV3_DACEMODE=Run
+srun python -m pace.driver.run ${JENKINS_DIR}/driver_configs/baroclinic_c768_96ranks.yaml
 EOF
 launch_job run.daint.slurm 15000
+
+mv 
 tar -czvf ${PACE_DIR}/output.tar.gz output.zarr
 
-module load sarus
-sarus pull elynnwu/pace:latest
-echo "####### generating figures..."
-srun -C gpu --partition=debug --account=s1053 --time=00:30:00 sarus run --mount=type=bind,source=${PACE_DIR},destination=/work elynnwu/pace:latest python /work/driver/examples/plot_pcolormesh_cube.py moist_baroclinic_c576 ua 40 --zarr_output=/work/output.zarr --diff_init --start=1 --stop=3 --force_symmetric_colorbar --size=576
-echo "####### figures completed."
