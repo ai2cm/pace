8beb42e673b0c7e6214c813c1bb79c8fa6cf6051
Elynn Wu <elynn510@gmail.com>
2022-12-19 15:45:39 -0800

    added reference

--- fv3core/pace/fv3core/stencils/divergence_damping.py
+++ fv3core/pace/fv3core/stencils/divergence_damping.py
@@ -615,6 +615,7 @@ class DivergenceDamping:
 
         da_min: float = self._get_da_min()
         if self._stretched_grid:
+            # reference https://github.com/NOAA-GFDL/GFDL_atmos_cubed_sphere/blob/main/model/sw_core.F90#L1422 # noqa: E501
             dd8 = da_min * self._d4_bg ** (self._nonzero_nord + 1)
         else:
             dd8 = (da_min_c * self._d4_bg) ** (self._nonzero_nord + 1)
